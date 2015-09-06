0a0f9797222a3879f5f7760a9ed2db8bd9048ace
Glenn R. Fisher <grfisher@us.ibm.com>
2015-09-05 20:57:03 -0500

    Add SpeechToText iOS app project.

---
+++ SpeechToText/SpeechToText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechToText
+//
+//  Created by Glenn Fisher on 9/5/15.
+//  Copyright (c) 2015 MIL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

be60dd5396a97ad71f8382d7c673707c524699bb
Glenn R. Fisher <grfisher@us.ibm.com>
2015-09-08 23:07:06 -0500

    Add SwiftyJSON library. Temporarily patch the upload() issue. Add documention.
    SwiftyJSON is now included in the source code. It is used to parse responses from the Watson Speech to Text service's REST api.
    
    The upload() function was failing because of a time-out error. As a result, no response was returned from the server. Sending a GET request immediately prior to upload()'s POST request often fixes the problem. It doesn't always seem to work, though, so there may be a race condition. The REST API also does not seem to require any communication prior to the POST, so the GET should be extraneous and eventually refactored in favor of a permanent solution.
    
    Extensive documentation was added to the WatsonSpeechToText.swift library, including links to helpful external documentation, an example call/response, a list of design decisions/questions, and a list of todo items.

--- SpeechToText/SpeechToText/WatsonSpeechToText.swift
+++ SpeechToText/SpeechToText/WatsonSpeechToText.swift
@@ -6,10 +6,78 @@
 //  Copyright (c) 2015 MIL. All rights reserved.
 //
 
-// Watson Documentation: http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/speech-to-text/
-// Alamofire Documentation: https://github.com/Alamofire/Alamofire
+/* Helpful Documentation:
 
-// todo: What conventions are used when setting the domain of an NSError?
+    1. Watson: http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/speech-to-text/
+    2. Alamofire: https://github.com/Alamofire/Alamofire
+    3. SwiftyJSON: https://github.com/SwiftyJSON/SwiftyJSON
+
+*/
+
+/* Example Call/Response:
+
+    Call:
+
+        1. SpeechToTextObject.transcribeAudio(pathToAudioFile) { transcript, error in ... }
+
+        2. curl -u "004db54a-c5e0-472b-a6eb-7b106fd31370":"o55eeuCST9YU" \
+                -H "content-type: audio/flac" --data-binary @"test-audio.flac" \
+                "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
+
+    Response:
+
+        {
+            "results": [
+                {
+                    "alternatives": [
+                        {
+                            "confidence": 0.869285523891449,
+                            "transcript": "several tornadoes touch down as a line of severe thunderstorms swept through colorado on sunday "
+                        }
+                    ],
+                    "final": true
+                }
+            ],
+            "result_index": 0
+        }
+
+*/
+
+/* Design Decisions and Questions:
+
+    1. Use callbacks for asynchronous network calls.
+
+    2. Rely on Swift 2.0 idioms (esp. error-handling).
+
+    3. Should we use 3rd-party libraries (Alamofire, SwiftyJSON) or roll our own?
+        - If we roll our own, should it expose functionality common across REST APIs?
+        - If we roll our own, should we contribute it to the open-source community?
+        - If we roll our own and open-source it, why not just support a current open-source project?
+
+    4. How much Speech to Text functionality should be exposed to the user?
+        - Get models (/v1/models)? -> Doesn't seem like a run-time decision.
+        - Get model details (/v1/models/{model_id})? -> Doesn't seem like a run-time decision.
+        - Session vs. sessionless modes? -> Expose session use cases, but hide management details.
+        - Multipart files? -> Should be supported somehow, for live/streaming client use cases.
+        - Return type? How much data should it expose? -> Enums with map convenience functions that can be chained?
+        - How should we expose varying amounts of complexity to the user? (e.g. per-word vs. per-transcription confidence?)
+
+*/
+
+/* TODO:
+    
+    1. Upgrade to Swift 2.0 idioms (esp. error-handling).
+    2. Research and follow conventions for setting the domain of NSError.
+    3. Update error-handling (and all code, really) to Swift 2.0.
+    4. Determine why the upload() function is troublesome.
+        - Why do we seem to need to establish a connection with request() first?
+        - Why does it not seem to work every time?
+        - Why does it almost seem like there is a race condition? (Perhaps with the GET request?)
+    5. Ensure audio files meet the service's size constraints.
+    6. Add support for l16 MIME-type.
+    7. Add support for additional functionality, in-line with design decisions above.
+
+*/
 
 import Foundation
 
@@ -34,7 +102,7 @@ public class WatsonSpeechToText {
     /** Transcribe an audio file. */
     public func transcribeFile(file: NSURL, completionHandler: (String?, NSError?) -> Void) {
         
-        // ensure audio file is supported by Watson
+        // ensure audio file format is supported by Watson
         let contentType = MIMETypeOfFile(file)
         if contentType == nil {
             let error = NSError(domain: "FileType", code: 415, userInfo: [NSLocalizedDescriptionKey:"Unsupported media type"])
@@ -42,20 +110,23 @@ public class WatsonSpeechToText {
             return
         }
         
-        // query Watson for transcript
+        // establish connection to Watson Speech to Text service
         let endpoint = "\(baseURL)/v1/recognize"
-        let headers = ["content-type":contentType!]
+        request(.GET, endpoint)
+            .authenticate(user: username, password: password)
+            .responseString() { _, _, _, _ in }
+
+        // query Watson for transcript
+        let headers = ["Content-Type": contentType!]
         upload(.POST, endpoint, headers: headers, file: file)
-            .authenticate(user: username, password: password, persistence: .None)
+            .authenticate(user: username, password: password)
             .validate(contentType: ["application/json"])
             .responseJSON() { _, _, body, error in
                 
-                println("This branch is never reached.") // todo: debugging
-                
                 // successful transcription
                 if let body: AnyObject = body {
-                    // let transcript = body["results"]["alternatives"]["transcript"]
-                    let transcript = "\(body)" // todo: debugging
+                    let json = JSON(body)
+                    let transcript = json["results"][0]["alternatives"][0]["transcript"].stringValue
                     completionHandler(transcript, nil)
                 }
                 
@@ -101,7 +172,6 @@ public class WatsonSpeechToText {
                 contentType = "audio/wav"
             case "flac":
                 contentType = "audio/flac"
-                // todo: what's l16?!
             default:
                 contentType = nil
             }

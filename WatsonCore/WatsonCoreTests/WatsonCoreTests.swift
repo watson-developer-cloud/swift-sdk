b9dd002a330c755d80d3b151e0e4cd515cdfd930
Glenn R. Fisher <grfisher@us.ibm.com>
2015-09-20 23:51:30 -0500

    Add test case for WatsonCore.WatsonAuthentication.

--- WatsonCore/WatsonCoreTests/WatsonCoreTests.swift
+++ WatsonCore/WatsonCoreTests/WatsonCoreTests.swift
@@ -11,6 +11,10 @@ import XCTest
 
 class WatsonCoreTests: XCTestCase {
     
+    // todo: remove authentication credentials
+    private let authentication = WatsonAuthentication(serviceURL: "https://stream.watsonplatform.net/speech-to-text/api", username: "004db54a-c5e0-472b-a6eb-7b106fd31370", password: "o55eeuCST9YU")
+    private let timeout: NSTimeInterval = 60.0
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -30,6 +34,33 @@ class WatsonCoreTests: XCTestCase {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+        }
+    }
+    
+    /**
+    Test WatsonAuthentication by ensuring a 200 response from a Watson Speech to Text endpoint that requires authentication.
+    */
+    func testAuthentication() {
+        
+        let expectation = expectationWithDescription("WatsonAuthentication")
+        
+        let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: sessionConfiguration, delegate: authentication, delegateQueue: nil)
+        let endpoint = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
+        let url = NSURL(string: endpoint)
+        if let url = url {
+            let dataTask = session.dataTaskWithURL(url) {
+                data, response, error in
+                let statusCode = (response as? NSHTTPURLResponse)?.statusCode
+                XCTAssert(statusCode == 200, "Status code not 200. (Check credentials.)")
+                expectation.fulfill()
+            }
+            dataTask.resume()
+        }
+        
+        waitForExpectationsWithTimeout(timeout) {
+            error in
+            XCTAssertNil(error, "Timeout")
         }
     }
     

f2d5b3bf5b32f83ecbac16721c0eee63132da2ac
Glenn R. Fisher <grfisher@us.ibm.com>
2015-09-21 05:34:40 -0500

    Add tests for WatsonSpeechToText.

--- WatsonSpeechToText/WatsonSpeechToTextTests/WatsonSpeechToTextTests.swift
+++ WatsonSpeechToText/WatsonSpeechToTextTests/WatsonSpeechToTextTests.swift
@@ -11,6 +11,10 @@ import XCTest
 
 class WatsonSpeechToTextTests: XCTestCase {
     
+    // todo: remove authentication credentials
+    private let stt = WatsonSpeechToText(username: "004db54a-c5e0-472b-a6eb-7b106fd31370", password: "o55eeuCST9YU")
+    private let timeout: NSTimeInterval = 60.0
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -30,6 +34,110 @@ class WatsonSpeechToTextTests: XCTestCase {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+        }
+    }
+    
+    /**
+    Test WatsonSpeechToText by ensuring that transcribeFile() does not return a nil transcription string for a known audio file.
+    */
+    func testTranscribeFileForNonNilResults() {
+        
+        let expectation = expectationWithDescription("Testing WatsonSpeechToText.transcribeFile() to ensure non-nil results.")
+        
+        let file = NSBundle(forClass: self.dynamicType).URLForResource("SpeechSample", withExtension: "flac")
+        stt.transcribeFile(file!) {
+            transcription, error in
+            XCTAssertNotNil(transcription, "Transcription is null.")
+            XCTAssertNil(error, "Error is not null.")
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(timeout) {
+            error in
+            XCTAssertNil(error, "Timeout")
+        }
+    }
+    
+    /**
+    Test WatsonSpeechToText by ensuring that transcribeFile() returns the expected transcription for a known audio file.
+    */
+    func testTranscribeFileForTranscription() {
+        
+        let expectation = expectationWithDescription("Testing WatsonSpeechToText.transcribeFile() to ensure matching transcription with sample file.")
+
+        let file = NSBundle(forClass: self.dynamicType).URLForResource("SpeechSample", withExtension: "flac")
+        stt.transcribeFile(file!) {
+            transcription, error in
+            
+            XCTAssertNotNil(transcription, "Transcription is nil.")
+            XCTAssertNil(error, "Error is not nil.")
+            
+            guard let transcription = transcription else {
+                expectation.fulfill()
+                return
+            }
+            
+            let match = (transcription == "several tornadoes touch down as a line of severe thunderstorms swept through Colorado on Sunday ")
+            XCTAssert(match, "Transcription result does not match expectation.")
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(timeout) {
+            error in
+            XCTAssertNil(error, "Timeout")
+        }
+    }
+    
+    /**
+    Test WatsonSpeechToText by ensuring that transcribeData() does not return a nil transcription string for known audio data.
+    */
+    func testTranscribeDataForNonNilResults() {
+        
+        let expectation = expectationWithDescription("Testing WatsonSpeechToText.transcribeData() to ensure non-nil results.")
+        
+        let file = NSBundle(forClass: self.dynamicType).URLForResource("SpeechSample", withExtension: "flac")
+        let data = NSData(contentsOfURL: file!)!
+        stt.transcribeData(data, contentType: "audio/flac") {
+            transcription, error in
+            XCTAssertNotNil(transcription, "Transcription is nil.")
+            XCTAssertNil(error, "Error is not nil.")
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(timeout) {
+            error in
+            XCTAssertNil(error, "Timeout")
+        }
+    }
+    
+    /**
+    Test WatsonSpeechToText by ensuring that transcribeData() returns the expected transcription for known audio data.
+    */
+    func testTranscribeDataForTranscription() {
+        
+        let expectation = expectationWithDescription("Testing WatsonSpeechToText.transcribeData() to ensure matching transcription with sample audio data.")
+        
+        let file = NSBundle(forClass: self.dynamicType).URLForResource("SpeechSample", withExtension: "flac")
+        let data = NSData(contentsOfURL: file!)!
+        stt.transcribeData(data, contentType: "audio/flac") {
+            transcription, error in
+            
+            XCTAssertNotNil(transcription, "Transcription is nil.")
+            XCTAssertNil(error, "Error is not nil.")
+            
+            guard let transcription = transcription else {
+                expectation.fulfill()
+                return
+            }
+            
+            let match = (transcription == "several tornadoes touch down as a line of severe thunderstorms swept through Colorado on Sunday ")
+            XCTAssert(match, "Transcription result does not match expectation.")
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(timeout) {
+            error in
+            XCTAssertNil(error, "Timeout")
         }
     }
     

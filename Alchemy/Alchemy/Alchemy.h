53282ad97558d38185785bf5eef99dfce6bd3d4b
Vincent Herrin <vherrin@us.ibm.com>
2015-09-30 10:01:47 -0500

    Initial commit for Alchemy but many more changes to come

---
+++ Alchemy/Alchemy/Alchemy.h
@@ -0,0 +1,19 @@
+//
+//  Alchemy.h
+//  Alchemy
+//
+//  Created by Vincent Herrin on 9/27/15.
+//  Copyright © 2015 MIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alchemy.
+FOUNDATION_EXPORT double AlchemyVersionNumber;
+
+//! Project version string for Alchemy.
+FOUNDATION_EXPORT const unsigned char AlchemyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alchemy/PublicHeader.h>
+
+
---
+++ Alchemy/Alchemy/Core.swift
@@ -0,0 +1,14 @@
+//
+//  Core.swift
+//  Alchemy
+//
+//  Created by Vincent Herrin on 9/27/15.
+//  Copyright © 2015 MIL. All rights reserved.
+//
+
+import Foundation
+
+public class Core : NSObject {
+    
+    
+}

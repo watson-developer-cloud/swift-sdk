6f8817149de1a58e77b58566ad14da65c9ea7677
Vincent Herrin <vherrin@us.ibm.com>
2015-10-04 19:36:18 -0500

    more changes to reflect a single project for each Vision Data and Language along with moving analyze to Core Data

---
+++ AlchemyVision/AlchemyVision/AlchemyVision.h
@@ -0,0 +1,19 @@
+//
+//  AlchemyVision.h
+//  AlchemyVision
+//
+//  Created by Vincent Herrin on 10/4/15.
+//  Copyright © 2015 MIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlchemyVision.
+FOUNDATION_EXPORT double AlchemyVisionVersionNumber;
+
+//! Project version string for AlchemyVision.
+FOUNDATION_EXPORT const unsigned char AlchemyVisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlchemyVision/PublicHeader.h>
+
+

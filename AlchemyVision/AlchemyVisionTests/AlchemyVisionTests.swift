0c73a45e6d03ab29ca38eca7366f1ca7fa98f772
Vincent Herrin <vherrin@us.ibm.com>
2015-10-14 19:49:01 -0500

    Set the test back and add some logging verbiage

--- AlchemyVision/AlchemyVisionTests/AlchemyVisionTests.swift
+++ AlchemyVision/AlchemyVisionTests/AlchemyVisionTests.swift
@@ -17,10 +17,10 @@ class AlchemyVisionTests: XCTestCase {
     
     var test_text = "this is a silly sentence to test the Node.js SDK"
     var test_html = "<html><head><title>The best SDK Test | AlchemyAPI</title></head><body><h1>Hello World!</h1><p>My favorite language is Javascript</p></body></html>"
-   // var test_url = "http://www.nytimes.com/2013/07/13/us/politics/a-day-of-friction-notable-even-for-a-fractious-congress.html?_r=0"
+    var test_url = "http://www.nytimes.com/2013/07/13/us/politics/a-day-of-friction-notable-even-for-a-fractious-congress.html?_r=0"
     
     //    var test_url = "https://www.google.com/search?q=cat&espv=2&source=lnms&tbm=isch&sa=X&ved=0CAcQ_AUoAWoVChMIuOev14miyAIVEeqACh0mOwhq&biw=1440&bih=805"
-        var test_url = "https://www.petfinder.com/wp-content/uploads/2012/11/138190243-cat-massage-632x475.jpg"
+    //    var test_url = "https://www.petfinder.com/wp-content/uploads/2012/11/138190243-cat-massage-632x475.jpg"
     //    var test_image = './emaxfpo.jpg';
     
     
@@ -41,22 +41,23 @@ class AlchemyVisionTests: XCTestCase {
     
     /**
     This will test an invalid API key for all three segments of Vision, Language and Data
-
+    */
     func testInvalidAPIKey() {
         
         let expectation = expectationWithDescription("Test Invalid API Key")
         
         let service : VisionImpl = VisionImpl( apiKey: "WRONG")
         
-        service.urlGetRankedImageKeywords(test_url, outputMode: AlchemyConstants.OutputMode.XML, forceShowAll: true, knowledgeGraph: 1, callback: { response, resultStatus in
+        service.urlGetRankedImageKeywords(test_url, forceShowAll: true, knowledgeGraph: 1, completionHandler: { response in
             
-            XCTAssertNil(response, "Response is not nil.")
+            XCTAssertEqual(response.totalTransactions, 0)
             
-            XCTAssertNotNil(resultStatus, "Error is nil.")
+            Log.sharedLogger.verbose("Add error handling in return value")
+      //      XCTAssertNotNil(resultStatus, "Error is nil.")
             
-            XCTAssertEqual(resultStatus.status, "ERROR")
+      //      XCTAssertEqual(resultStatus.status, "ERROR")
             
-            XCTAssertEqual(resultStatus.statusInfo, "invalid-api-key")
+      //      XCTAssertEqual(resultStatus.statusInfo, "invalid-api-key")
             
             expectation.fulfill()
         })
@@ -72,64 +73,12 @@ class AlchemyVisionTests: XCTestCase {
             
             XCTAssertNotNil(response, "Response is nil.")
             
+            Log.sharedLogger.verbose("add result return value ")
           //  XCTAssertNotNil(resultStatus, "Error is nil.")
             
-            XCTAssertEqual(resultStatus.status, "OK")
+     //       XCTAssertEqual(resultStatus.status, "OK")
             
-            XCTAssertEqual(resultStatus.statusInfo, "")
-            
-            XCTAssertGreaterThan(response.totalTransactions, 3)
-            
-            XCTAssertEqual(response.imageKeyWords.count, 1)
-            
-            XCTAssertEqual(response.imageKeyWords[0].text, "person")
-            
-            XCTAssertGreaterThan(response.imageKeyWords[0].score, 0.90)
-            
-            // add some logic here for testing
-            expectation.fulfill()
-        })
-    
-        waitForExpectationsWithTimeout(timeout, handler: { error in XCTAssertNil(error, "Timeout") })
-    }
-*/
-    func testAlamo(){
-        
-        let expectation = expectationWithDescription("Test URLGetRankedImageKeywords")
-        
-        serviceVision.urlGetRankedImageKeywords(test_url, forceShowAll: true, knowledgeGraph: 1, completionHandler: { response in
-            
-            // add some logic here for testing
-            expectation.fulfill()
-        })
-
-        
-        /*
-        
-        serviceVision.urlGetRankedImageKeywords(test_url, forceShowAll: true, knowledgeGraph: 1, callback: { response, resultStatus in
-            
-            // add some logic here for testing
-            expectation.fulfill()
-        })
-        */
-        waitForExpectationsWithTimeout(timeout, handler: { error in XCTAssertNil(error, "Timeout") })
-    }
-    
-    /*
-    
-    func testURLGetRankedImageKeywordsJSON(){
-        
-        let expectation = expectationWithDescription("Test URLGetRankedImageKeywords")
-        
-        serviceVision.urlGetRankedImageKeywords(test_url, outputMode: AlchemyConstants.OutputMode.JSON, forceShowAll: true, knowledgeGraph: 1, callback: { response, resultStatus in
-            
-            XCTAssertNotNil(response, "Response is nil.")
-            
-            XCTAssertNotNil(resultStatus, "Error is nil.")
-            
-            XCTAssertEqual(resultStatus.status, "OK")
-            
-            XCTAssertEqual(resultStatus.statusInfo, "")
+     //       XCTAssertEqual(resultStatus.statusInfo, "")
             
             XCTAssertGreaterThan(response.totalTransactions, 3)
             
@@ -142,9 +91,7 @@ class AlchemyVisionTests: XCTestCase {
             // add some logic here for testing
             expectation.fulfill()
         })
-        
+    
         waitForExpectationsWithTimeout(timeout, handler: { error in XCTAssertNil(error, "Timeout") })
     }
-*/
-    
 }

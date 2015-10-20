eda02187dc8ac3f397dfca09ec21b62fc5fecbcb
Vincent Herrin <vherrin@us.ibm.com>
2015-10-20 16:28:45 -0500

    - make sure that model can populate itself - a little cleanup of code

--- AlchemyVision/AlchemyVision/Models/ImageKeyWord.swift
+++ AlchemyVision/AlchemyVision/Models/ImageKeyWord.swift
@@ -11,14 +11,23 @@ import WatsonCore
 import SwiftyJSON
 
 
+private enum ImageKeyWordEnum:String {
+    case Text = "text"
+    case Score = "score"
+}
+
 public struct ImageKeyWord {
 
-    public let text: String
-    public let score: Double
+    var text = ""
+    var score = 0.0
     
     init(text: String, score: Double) {
-        
         self.text = text
         self.score = score
+    }
+    
+    init(json: JSON) {
+        text    = json[ImageKeyWordEnum.Text.rawValue].stringValue
+        score   = json[ImageKeyWordEnum.Score.rawValue].doubleValue
     }
 }

9e9642390c1167e42bfadd5f662ee1e1a51bb05b
Glenn Fisher <glennfisher@Glenns-MacBook-Pro.local>
2015-11-06 09:37:40 -0600

    Add SpeechToText project
    Removes the existing WatsonSpeechToText project and adds a new SpeechToText project.
    
    SpeechToText will be re-written to utilize the updated WatsonCore and conform with the best practices learned from LanguageTranslation.
    
    Note, however, that Swift files from the WatsonSpeechToText project were copied into the new SpeechToText project. They exist for reference only, and will be deleted as SpeechToText progresses.

---
+++ SpeechToText/SpeechToText/SpeechToText.swift
@@ -0,0 +1,9 @@
+//
+//  SpeechToText.swift
+//  SpeechToText
+//
+//  Created by Glenn Fisher on 11/6/15.
+//  Copyright © 2015 IBM Mobile Innovation Lab. All rights reserved.
+//
+
+import Foundation

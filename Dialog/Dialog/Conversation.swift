82304d8aea643f9e4248f52e14aaff0dbb057cf1
Jonathan Ballands <jballands@gmail.com>
2015-11-09 10:12:43 -0600

    Got started stubbing out the dialog service

---
+++ Dialog/Dialog/Conversation.swift
@@ -0,0 +1,11 @@
+//
+//  Conversation.swift
+//  Dialog
+//
+//  Created by Jonathan Ballands on 11/9/15.
+//  Copyright © 2015 Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation
+import WatsonCore
+

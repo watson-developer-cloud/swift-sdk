bd4e896ef415ba7767ed5ae08ae3ec323417788b
Glenn Fisher <glennfisher@Glenns-MacBook-Pro.local>
2015-11-09 09:11:36 -0600

    Update bundle identifiers
    From: `com.ibm.watson.developer-cloud.ServiceName`
    To: `com.ibm.watson.developer-cloud.service-name`
    
    Also includes the addition of the SocketIO framework to the SpeechToText project.

--- SpeechToText/SpeechToText/SpeechToText.swift
+++ SpeechToText/SpeechToText/SpeechToText.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import SocketIO
+
+public class SpeechToText {
+    
+    public func transcribe(audio: NSURL, callback: (String?) -> Void) {
+        let socket = SocketIOClient(socketURL: "localhost:8080", options: [.Log(true), .ForcePolling(true)])
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("currentAmount") {data, ack in
+            if let cur = data[0] as? Double {
+                socket.emitWithAck("canUpdate", cur)(timeoutAfter: 0) {data in
+                    socket.emit("update", ["amount": cur + 2.50])
+                }
+                
+                ack?.with("Got your currentAmount", "dude")
+            }
+        }
+        
+        socket.connect()
+    }
+}

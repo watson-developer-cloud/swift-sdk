7c78bf5f314aae861728739e54dd04ffc64776b8
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-09 09:31:10 -0600

    revises stubs

--- AlchemyLanguage/AlchemyLanguage/AlchemyLanguage.swift
+++ AlchemyLanguage/AlchemyLanguage/AlchemyLanguage.swift
@@ -12,154 +12,29 @@ import WatsonCore
 
 class AlchemyLanguage: Service {}
 
-
 /**
 
-MARK: URL
+MARK: Language Detection
 
 */
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Image Tagging
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Image
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Face Detection
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Sentiment
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Text
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: HTML
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Taxonomy
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Concepts
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Entities
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Keywords
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Relations
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Text Extraction
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Title Extraction
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Image Link Extraction
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Authors
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Microformats
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Publication Date
-
-*/
-extension AlchemyLanguage { }
-
-
-/**
-
-MARK: Language
-
-*/
-extension AlchemyLanguage { }
+public extension AlchemyLanguage {
+
+    public func URLGetLanguage() {
+        
+        
+        
+    }
+    
+    public func HTMLGetLanguage() {
+        
+        
+        
+    }
+    
+    public func TextGetLanguage() {
+        
+        
+        
+    }
+    
+}

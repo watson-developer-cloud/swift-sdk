50dd022ea813f168ce9eb8332f94b1034affa780
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-10 21:09:11 -0600

    AlchemyLanguageConstants stub

---
+++ AlchemyLanguage/AlchemyLanguage/AlchemyLanguageConstants.swift
@@ -0,0 +1,9 @@
+//
+//  AlchemyLanguageConstants.swift
+//  AlchemyLanguage
+//
+//  Created by Ruslan Ardashev on 11/10/15.
+//  Copyright © 2015 ibm.mil. All rights reserved.
+//
+
+import Foundation

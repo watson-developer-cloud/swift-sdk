a9ecfe68bade9da0b68cbc1783a8c80f8fa2d985
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-16 15:29:43 -0600

    DocumentAuthor model class stub

---
+++ WatsonSDK/AlchemyLanguage/Models/DocumentAuthor.swift
@@ -0,0 +1,9 @@
+//
+//  DocumentAuthor.swift
+//  WatsonSDK
+//
+//  Created by Ruslan Ardashev on 11/16/15.
+//  Copyright © 2015 IBM Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation

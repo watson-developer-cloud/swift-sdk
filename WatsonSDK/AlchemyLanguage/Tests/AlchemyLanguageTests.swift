eccd7c8602d9d88411183de41629f7abae01ccc0
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-16 16:32:53 -0600

    test method stubs

--- WatsonSDK/AlchemyLanguage/Tests/AlchemyLanguageTests.swift
+++ WatsonSDK/AlchemyLanguage/Tests/AlchemyLanguageTests.swift
@@ -14,16 +14,22 @@ class AlchemyLanguageTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testInvalidHTMLGetAuthor() {
+        
+        
+        
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testURLGetAuthor() {
+        
+        
+        
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInvalidURLGetAuthor() {
+        
+        
+        
     }
     
     func testPerformanceExample() {

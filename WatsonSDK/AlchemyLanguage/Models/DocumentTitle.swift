2dcc6c8487fa57033e99d64a2753360d40f00743
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-17 16:02:11 -0600

    stubs for DocumentText and Title

---
+++ WatsonSDK/AlchemyLanguage/Models/DocumentTitle.swift
@@ -0,0 +1,9 @@
+//
+//  DocumentTitle.swift
+//  WatsonSDK
+//
+//  Created by Ruslan Ardashev on 11/17/15.
+//  Copyright © 2015 IBM Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation

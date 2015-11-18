298ddf1e52f4e838c28a90b9fbd72f9871671549
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-18 11:26:59 -0600

    stubs files

---
+++ WatsonSDK/AlchemyLanguage/Utils/AlchemyCombineDictionaryUtil.swift
@@ -0,0 +1,9 @@
+//
+//  AlchemyCombineDictionaryUtil.swift
+//  WatsonSDK
+//
+//  Created by Ruslan Ardashev on 11/18/15.
+//  Copyright © 2015 IBM Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation

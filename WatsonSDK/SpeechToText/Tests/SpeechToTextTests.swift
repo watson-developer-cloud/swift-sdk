3a7e8aa9dda75250bcdbe9efe4609d8a506152d2
Glenn R. Fisher <grfisher@us.ibm.com>
2015-11-19 09:19:46 -0600

    Perform a recognition request with SpeechToText

--- WatsonSDK/SpeechToText/Tests/SpeechToTextTests.swift
+++ WatsonSDK/SpeechToText/Tests/SpeechToTextTests.swift
@@ -11,12 +11,21 @@ import XCTest
 
 class SpeechToTextTests: XCTestCase {
     
-    /// Timeout for an asynchronous call to return before failing the unit test
-    private let timeout: NSTimeInterval = 60.0
+    private let service = SpeechToText()
+    private var inputText: String?
+    private let timeout: NSTimeInterval = 30.0
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        if let url = NSBundle(forClass: self.dynamicType).pathForResource("Credentials", ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: url) as? Dictionary<String, String> {
+                service.setUsernameAndPassword(dict["SpeechToTextUsername"]!, password: dict["SpeechToTextPassword"]!)
+            } else {
+                XCTFail("Unable to extract dictionary from plist")
+            }
+        } else {
+            XCTFail("Plist file not found")
+        }
     }
     
     override func tearDown() {
@@ -24,26 +33,12 @@ class SpeechToTextTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testWebsockets() {
         let expectation = expectationWithDescription("WebSockets")
-        let service = SpeechToText(username: "***REMOVED***", password: "***REMOVED***")
         service.transcribe(NSBundle(forClass: self.dynamicType).URLForResource("SpeechSample", withExtension: "flac")!) {
             transcription in
             expectation.fulfill()
         }
-        // sleep(2)
         waitForExpectationsWithTimeout(timeout) {
             error in XCTAssertNil(error, "Timeout")
         }

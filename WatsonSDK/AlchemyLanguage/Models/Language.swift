1a5ea959f73b89b6bb506b0edaacd1b99fc73fb9
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-11-24 16:14:17 -0600

    stubs for model classes

---
+++ WatsonSDK/AlchemyLanguage/Models/Language.swift
@@ -0,0 +1,9 @@
+//
+//  Language.swift
+//  WatsonSDK
+//
+//  Created by Ruslan Ardashev on 11/24/15.
+//  Copyright © 2015 IBM Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation

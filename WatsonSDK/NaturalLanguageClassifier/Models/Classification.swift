2b7e3e5ec90aa96cbb81b2078fe6d951cbb32645
Vincent Herrin <vherrin@us.ibm.com>
2015-12-01 13:19:24 -0600

    IBM-MIL/Watson-iOS-SDK #82 - Natural Language Classifier - Add a topConfidence field for the Classification object  FIX: Added computed field that grabs the top confidence class and gets that confidence value.

--- WatsonSDK/NaturalLanguageClassifier/Models/Classification.swift
+++ WatsonSDK/NaturalLanguageClassifier/Models/Classification.swift
@@ -10,20 +10,31 @@ import Foundation
 import ObjectMapper
 
 public struct Classification: Mappable {
-
-  var id: String?
-  var url: String?
-  var text: String?
-  var topClass: String?
-  var classes: [ClassifiedClass]?
-  
-  public init?(_ map: Map) {}
-  
-  public mutating func mapping(map: Map) {
-    id        <- map["classifier_id"]
-    text      <- map["text"]
-    url       <- map["url"]
-    topClass  <- map["top_class"]
-    classes   <- map["classes"]
-  }
+    var id: String?
+    var url: String?
+    var text: String?
+    var topClass: String?
+    var topConfidence: Double? {
+        guard let classes = self.classes else {
+            return nil
+        }
+        var double = 0.0
+        for classifiedClass in classes {
+            if(classifiedClass.name == topClass) {
+                double = classifiedClass.confidence!
+            }
+        }
+        return double
+    }
+    var classes: [ClassifiedClass]?
+    
+    public init?(_ map: Map) {}
+    
+    public mutating func mapping(map: Map) {
+        id        <- map["classifier_id"]
+        text      <- map["text"]
+        url       <- map["url"]
+        topClass  <- map["top_class"]
+        classes   <- map["classes"]
+    }
 }

de2b7562c6371ba7f6fe8a58da5f6760c72357ac
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-12-01 15:59:21 -0600

    file stubs

---
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/BarView/BarViewDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  BarViewParentProtocol.swift
+//  WatsonSDK-DemoApplication
+//
+//  Created by Ruslan Ardashev on 12/1/15.
+//  Copyright © 2015 ibm.mil. All rights reserved.
+//
+
+import Foundation
+
+protocol BarViewParentProtocol {
+    
+    func presentSelect()
+    func presentSettings()
+    
+}
---
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/ChildViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChildViewController.swift
+//  WatsonSDK-DemoApplication
+//
+//  Created by Ruslan Ardashev on 12/1/15.
+//  Copyright © 2015 ibm.mil. All rights reserved.
+//
+
+import Foundation

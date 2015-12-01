a2720fff7450fb8e597ea3e1f4c067a5d713c221
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-12-01 15:19:46 -0600

    sample app root view controller progress

--- Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/ViewController.swift
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // sizing
+    private var screenBounds: CGRect { return UIScreen.mainScreen().bounds }
+    var screenWidth: CGFloat { return screenBounds.width }
+    var screenHeight: CGFloat { return screenBounds.height }
+    
+    // width
+    var popoverWidth: CGFloat { return 1.0 * screenWidth }
+    
+    // height
+    var barHeight: CGFloat { return 40.0 }
+    var childScreenHeight : CGFloat { return screenHeight - barHeight }
+    
+    // ui components
+    private var barView: BarView!
+    private var childViewContainer: UIView!
+    
+    var childViewController: UIViewController? {
+        
+        didSet {
+            
+            
+            
+        }
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        configureBarView()
+        
+        configureChildView()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func configureBarView() {
+        
+        self.barView = NSBundle.mainBundle().loadNibNamed("BarView", owner: self, options: nil).first! as! BarView
+        
+        let barViewFrame = CGRect(
+            x: 0.0,
+            y: childScreenHeight,
+            width: screenWidth,
+            height: barHeight
+        )
+        
+        self.barView.frame = barViewFrame
+        
+        self.view.addSubview(self.barView)
+        
     }
-
+    
+    // TODO: implement
+    private func configureChildView() {}
 
 }
 

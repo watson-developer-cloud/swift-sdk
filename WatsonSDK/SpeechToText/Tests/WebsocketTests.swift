b42ba0b495fdf2eaa8a7b7da2ff025b598318ac6
Robert F. Dickerson <rfdickerson@gmail.com>
2015-12-03 13:27:30 -0600

    Added a socket delegate

---
+++ WatsonSDK/SpeechToText/Tests/WebsocketTests.swift
@@ -0,0 +1,9 @@
+//
+//  WebsocketTests.swift
+//  WatsonSDK
+//
+//  Created by Robert Dickerson on 12/3/15.
+//  Copyright © 2015 IBM Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation

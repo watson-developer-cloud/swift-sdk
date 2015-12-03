f22a94feb34dc6ac01d114852629a4f4ee19c1d4
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-12-03 09:43:34 -0600

    stubs table view cell classes for selection and settings screens

---
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/SelectionView/SelectionTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SelectionTableViewCell.swift
+//  WatsonSDK-DemoApplication
+//
+//  Created by Ruslan Ardashev on 12/3/15.
+//  Copyright © 2015 ibm.mil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SelectionTableViewCell: UITableViewCell {
+    
+    
+    
+}

a328120571ff52b1b4366061be57e200b406238d
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-12-08 16:48:02 -0600

    builds on AL in demo app

---
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/Children/AlchemyLanguageSampleApp/AlchemyLanguageRequestViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AlchemyLanguageRequestViewController.swift
+//  WatsonSDK-DemoApplication
+//
+//  Created by Ruslan Ardashev on 12/8/15.
+//  Copyright © 2015 ibm.mil. All rights reserved.
+//
+
+import Foundation

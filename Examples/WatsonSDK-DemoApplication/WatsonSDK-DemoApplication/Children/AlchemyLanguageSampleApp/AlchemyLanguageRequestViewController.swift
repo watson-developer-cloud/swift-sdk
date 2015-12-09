dada0780a38333e18df8b79e7c62fedd19c7b989
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-12-09 12:07:37 -0600

    [sloppy commit] demo app progress

--- Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/Children/AlchemyLanguageSampleApp/AlchemyLanguageRequestViewController.swift
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/Children/AlchemyLanguageSampleApp/AlchemyLanguageRequestViewController.swift
@@ -12,11 +12,28 @@ import WatsonSDK
 
 class AlchemyLanguageRequestViewController: UIViewController {
     
-    @IBAction func closeButton() {
+    @IBAction func closeButtonPress() {
         
         self.dismissViewControllerAnimated(true, completion: nil)
         
     }
+    
+    @IBOutlet weak var closeButton: UIButton!
+    
+    @IBOutlet weak var entitiesLabel: UILabel!
+    @IBOutlet weak var entitiesEntry0: UILabel!
+    @IBOutlet weak var entitiesEntry1: UILabel!
+    @IBOutlet weak var entitiesEntry2: UILabel!
+    
+    @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var sentimentEntry0: UILabel!
+    @IBOutlet weak var sentimentEntry1: UILabel!
+    
+    @IBOutlet weak var keywordsLabel: UILabel!
+    @IBOutlet weak var keywordsEntry0: UILabel!
+    @IBOutlet weak var keywordsEntry1: UILabel!
+    @IBOutlet weak var keywordsEntry2: UILabel!
+    
     
     var _requestType: AlchemyLanguageConstants.RequestType!
     var requestType: AlchemyLanguageConstants.RequestType! {
@@ -24,5 +41,39 @@ class AlchemyLanguageRequestViewController: UIViewController {
         set { _requestType = newValue }
     }
     
+    var _requestString: String!
+    var requestString: String! {
+        get { assert(_requestString != nil, "Set AlchemyLanguageRequestViewController requestString before displaying."); return _requestString }
+        set { _requestString = newValue }
+    }
+    
+    // sizing
+    var screenBounds: CGRect { return UIScreen.mainScreen().bounds }
+    var screenWidth: CGFloat { return screenBounds.size.width }
+    var screenHeight: CGFloat { return screenBounds.size.height }
+    
+    var activeOriginY: CGFloat { return closeButton.frame.origin.y + closeButton.frame.size.height }
+    var activeHeight: CGFloat { return screenHeight - activeOriginY }
+    
+    // area of screen to use below X button
+    var activeFrame: CGRect {
+        
+        return CGRect(
+            x: 0.0,
+            y: activeOriginY,
+            width: screenWidth,
+            height: activeHeight
+        )
+        
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        
+        
+    }
     
 }
--- Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/Children/AlchemyLanguageSampleApp/AlchemyLanguageViewController.swift
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/Children/AlchemyLanguageSampleApp/AlchemyLanguageViewController.swift
@@ -36,29 +36,20 @@ extension AlchemyLanguageViewController {
 class AlchemyLanguageViewController: UIViewController {
 
     // ibactions
-    @IBAction func goURLButtonPress() { presentResponseViewControllerWithRequestType(.URL) }
-    @IBAction func goTextButtonPress() { presentResponseViewControllerWithRequestType(.Text) }
-    
-    private func presentResponseViewControllerWithRequestType(requestType: alcs.RequestType) {
-        
-        let sb = UIStoryboard(name: "AlchemyLanguageRequestViewController", bundle: nil)
-
-        let requestVC = sb.instantiateViewControllerWithIdentifier("AlchemyLanguageRequestViewController") as! AlchemyLanguageRequestViewController
-        requestVC.requestType = requestType
-        
-        self.presentViewController(requestVC, animated: true, completion: nil)
-        
-    }
-    
+    @IBAction func goURLButtonPress() { presentResponseViewControllerWithRequestType(.URL, requestString: self.urlTextField.text) }
+    @IBAction func goTextButtonPress() { presentResponseViewControllerWithRequestType(.Text, requestString: self.textTextField.text) }
     
     // iboutlets
     @IBOutlet weak var goURLButton: UIButton!
     @IBOutlet weak var goTextButton: UIButton!
     
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var textTextField: UITextField!
     
-    // properties
     // title animation
     var titleAnimation: SKView!
+    
+    let animationDuration = NSTimeInterval(0.75)
     
     
     // setup
@@ -88,6 +79,7 @@ class AlchemyLanguageViewController: UIViewController {
         
         let layer = button.layer
         
+        layer.cornerRadius = 5.0
         layer.borderColor = backgroundBlue.CGColor
         layer.backgroundColor = backgroundBlue.CGColor
         
@@ -107,6 +99,41 @@ class AlchemyLanguageViewController: UIViewController {
         
     }
     
-    
+    private func presentResponseViewControllerWithRequestType(requestType: alcs.RequestType, requestString: String!) {
+        
+        if requestString != "" {
+            
+            let sb = UIStoryboard(name: "AlchemyLanguageRequestViewController", bundle: nil)
+            
+            let requestVC = sb.instantiateViewControllerWithIdentifier("AlchemyLanguageRequestViewController") as! AlchemyLanguageRequestViewController
+            requestVC.requestType = requestType
+            requestVC.requestString = requestString
+            requestVC.view.alpha = 0.0
+            requestVC.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+            
+            self.presentViewController(requestVC, animated: false) {
+                
+                UIView.animateWithDuration(self.animationDuration) {
+                    
+                    requestVC.view.alpha = 1.0
+                    
+                }
+                
+            }
+            
+        } else {
+            
+            let alertController = UIAlertController(
+                title: "Content Required",
+                message:"Please enter an input string.",
+                preferredStyle: .Alert
+            )
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+            self.presentViewController(alertController, animated: true){ }
+            
+        }
+        
+    }
     
 }

49c5288fd0d0477da5991205b7c4f68d7a3ff0b8
Glenn R. Fisher <grfisher@us.ibm.com>
2015-12-09 21:48:54 -0600

    Update PersonalityInsights to use Alamofire routers

--- WatsonSDK/PersonalityInsights/Models/ContentItem.swift
+++ WatsonSDK/PersonalityInsights/Models/ContentItem.swift
@@ -17,53 +17,57 @@
 import Foundation
 import ObjectMapper
 
-/**
- *  An input model object that is used to pass in detailed information about content into the PersonalityInsights service
- */
-public struct ContentItem: Mappable {
+extension PersonalityInsights {
+    
+    /// An input model object used to pass detailed information about content items.
+    public struct ContentItem: Mappable {
 
-    var ID:String?
-    var userID: String?
-    var sourceID: String?
-    var created: Int?
-    var updated: Int?
-    var contentType: String?
-    var charset: String?
-    var language: String?
-    var content: String?
-    var parentID: String?
-    var reply: Bool?
-    var forward: Bool?
-    
-    public init?(_ map: Map) {}
-    
-    public init(ID:String?, userID:String?, sourceID:String?, created:Int?, updated:Int?, contentType:String?, charset:String?, language:String?, content:String?, parentID:String?, reply:Bool?, forward:Bool?) {
-        self.ID = ID
-        self.userID = userID
-        self.sourceID = sourceID
-        self.created = created
-        self.updated = updated
-        self.contentType = contentType
-        self.charset = charset
-        self.language = language
-        self.content = content
-        self.parentID = parentID
-        self.reply = reply
-        self.forward = forward
-    }
-    
-    public mutating func mapping(map: Map) {
-        ID          <- map["id"]
-        userID      <- map["userid"]
-        sourceID    <- map["sourceid"]
-        created     <- map["created"]
-        updated     <- map["updated"]
-        contentType <- map["contenttype"]
-        charset     <- map["charset"]
-        language    <- map["language"]
-        content     <- map["content"]
-        parentID    <- map["parentid"]
-        reply       <- map["reply"]
-        forward     <- map["forward"]
+        var ID:String?
+        var userID: String?
+        var sourceID: String?
+        var created: Int?
+        var updated: Int?
+        var contentType: String?
+        var charset: String?
+        var language: String?
+        var content: String?
+        var parentID: String?
+        var reply: Bool?
+        var forward: Bool?
+        
+        public init?(_ map: Map) {}
+        
+        public init(ID:String?, userID:String?, sourceID:String?, created:Int?,
+            updated:Int?, contentType:String?, charset:String?, language:String?,
+            content:String?, parentID:String?, reply:Bool?, forward:Bool?) {
+                
+            self.ID = ID
+            self.userID = userID
+            self.sourceID = sourceID
+            self.created = created
+            self.updated = updated
+            self.contentType = contentType
+            self.charset = charset
+            self.language = language
+            self.content = content
+            self.parentID = parentID
+            self.reply = reply
+            self.forward = forward
+        }
+        
+        public mutating func mapping(map: Map) {
+            ID          <- map["id"]
+            userID      <- map["userid"]
+            sourceID    <- map["sourceid"]
+            created     <- map["created"]
+            updated     <- map["updated"]
+            contentType <- map["contenttype"]
+            charset     <- map["charset"]
+            language    <- map["language"]
+            content     <- map["content"]
+            parentID    <- map["parentid"]
+            reply       <- map["reply"]
+            forward     <- map["forward"]
+        }
     }
 }

3c01e42e94bac04db31f35b3f9b65d02e5ea8333
Vincent Herrin <vherrin@us.ibm.com>
2015-12-11 09:36:43 -0600

    Add bash header in genDocs Remove command response in frameworks

--- genDocs.sh
+++ genDocs.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 #  jazzy -a IBM -m WatsonSDK -g https://github.com/IBM-MIL/Watson-iOS-SDK
-jazzy -m WatsonSDK -g https://github.com/IBM-MIL/Watson-iOS-SDK
+jazzy -m WatsonSDK -g https://github.com/IBM-MIL/Watson-iOS-SDKl

c9a1fcc36b51863bf041fe99340bc7340c8ebb95
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2015-12-14 10:00:59 -0600

    stubs own ALResultsView

---
+++ Examples/WatsonSDK-DemoApplication/WatsonSDK-DemoApplication/Children/AlchemyLanguageSampleApp/AlchemyLanguageResultsView/AlchemyLanguageResultsView.swift
@@ -0,0 +1,9 @@
+//
+//  AlchemyLanguageResultsView.swift
+//  WatsonSDK-DemoApplication
+//
+//  Created by Ruslan Ardashev on 12/14/15.
+//  Copyright © 2015 ibm.mil. All rights reserved.
+//
+
+import Foundation

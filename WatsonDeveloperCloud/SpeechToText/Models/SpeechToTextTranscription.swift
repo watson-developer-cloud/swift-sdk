95583a14bb44448eaf39da2d038998a305846f8b
Glenn R. Fisher <grfisher@us.ibm.com>
2016-03-03 12:09:37 -0600

    Use transforms to deserialize SpeechToTextTranscription

--- WatsonDeveloperCloud/SpeechToText/Models/SpeechToTextTranscription.swift
+++ WatsonDeveloperCloud/SpeechToText/Models/SpeechToTextTranscription.swift
@@ -28,20 +28,20 @@ public struct SpeechToTextTranscription: Mappable {
     public var confidence: Double?
 
     /// Timestamps for each word of the transcript.
-    public var timestamps: [SpeechToTextWordTimestamp]? // TODO: needs transform from ["word", 0.1, 0.2] to WordTimestamp object...
+    public var timestamps: [SpeechToTextWordTimestamp]?
 
     /// Confidence scores for each word of the transcript, between 0 and 1. Available only
     /// for the best alternative and only in results marked as final.
-    public var wordConfidence: [SpeechToTextWordConfidence]? // TODO: needs a transform from ["hello", 0.95] to WordConfidence object...
+    public var wordConfidence: [SpeechToTextWordConfidence]?
 
     /// Used internally to initialize a `SpeechToTextTranscription` from JSON.
     public init?(_ map: Map) { }
 
     /// Used internally to serialize and deserialize JSON.
     public mutating func mapping(map: Map) {
-        transcript     <- map["transcript"]
-        confidence     <- map["confidence"]
-        timestamps     <- map["timestamps"]
-        wordConfidence <- map["word_confidence"]
+        transcript     <-  map["transcript"]
+        confidence     <-  map["confidence"]
+        timestamps     <- (map["timestamps"], SpeechToTextWordTimestampTransform())
+        wordConfidence <- (map["word_confidence"], SpeechToTextWordConfidenceTransform())
     }
 }

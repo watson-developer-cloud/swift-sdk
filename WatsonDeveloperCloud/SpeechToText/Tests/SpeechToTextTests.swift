4c948b00f9fd051dc1a05992949d5474257be968
Glenn R. Fisher <grfisher@us.ibm.com>
2016-03-06 22:59:40 -0600

    Use default model for now (fix this bug!)

--- WatsonDeveloperCloud/SpeechToText/Tests/SpeechToTextTests.swift
+++ WatsonDeveloperCloud/SpeechToText/Tests/SpeechToTextTests.swift
@@ -171,7 +171,7 @@ class SpeechToTextTests: XCTestCase {
         }
 
         var settings = SpeechToTextSettings(contentType: format)
-        settings.model = "en-US_BroadbandModel" // TODO: Add this back and fix bug.
+        // settings.model = "en-US_BroadbandModel" // TODO: Add this back and fix bug.
         settings.learningOptOut = true
         settings.continuous = true
         settings.inactivityTimeout = -1

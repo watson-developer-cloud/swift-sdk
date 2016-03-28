4152f179f7ab27c5e669409f856e9d55e77bc915
Glenn R. Fisher <grfisher@us.ibm.com>
2016-03-28 11:40:57 -0500

    [text-to-speech] use wav instead of opus

--- WatsonDeveloperCloud/TextToSpeech/TextToSpeech.swift
+++ WatsonDeveloperCloud/TextToSpeech/TextToSpeech.swift
@@ -88,15 +88,15 @@ public class TextToSpeech: WatsonService {
             serviceURL: Constants.serviceURL,
             endpoint: Constants.synthesize,
             authStrategy: authStrategy,
-            accept: .OPUS,
+            accept: .WAV,
             urlParams: urlParams)
         
         // execute request
         gateway.request(request, serviceError: TextToSpeechError()) { data, error in
             if let data = data {
-                let pcm = self.opus.opusToPCM(data, sampleRate: self.defaultSampleRate)
-                let wave = self.addWaveHeader(pcm)
-                completionHandler(wave, error)
+                let wav = NSMutableData(data: data)
+                self.repairWAVHeader(wav)
+                completionHandler(wav, error)
             } else {
                 completionHandler(nil, error)
             }
@@ -195,5 +195,76 @@ public class TextToSpeech: WatsonService {
         newWavData.appendData(data)
         
         return newWavData
+    }
+
+    /**
+     Convert a big-endian byte buffer to a UTF-8 encoded string.
+
+     - parameter data: The byte buffer that contains a big-endian UTF-8 encoded string.
+     - parameter offset: The location within the byte buffer where the string begins.
+     - parameter length: The length of the string (without a null-terminating character).
+
+     - returns: A String initialized by converting the given big-endian byte buffer into
+     Unicode characters using a UTF-8 encoding.
+     */
+    private func dataToUTF8String(data: NSData, offset: Int, length: Int) -> String? {
+        let range = NSMakeRange(offset, length)
+        let subdata = data.subdataWithRange(range)
+        return String(data: subdata, encoding: NSUTF8StringEncoding)
+    }
+
+    /**
+     Convert a little-endian byte buffer to a UInt32 integer.
+
+     - parameter data: The byte buffer that contains a little-endian 32-bit unsigned integer.
+     - parameter offset: The location within the byte buffer where the integer begins.
+
+     - returns: An Int initialized by converting the given little-endian byte buffer into
+     an unsigned 32-bit integer.
+     */
+    private func dataToUInt32(data: NSData, offset: Int) -> Int {
+        var num: UInt32 = 0
+        let length = 4
+        let range = NSMakeRange(offset, length)
+        data.getBytes(&num, range: range)
+        return Int(num)
+    }
+
+    /**
+     Repair the WAV header for a WAV-formatted audio file produced by Watson Text to Speech.
+
+     - parameter data: The WAV-formatted audio file produced by Watson Text to Speech. The
+     byte data will be analyzed and repaired in-place.
+     */
+    private func repairWAVHeader(data: NSMutableData) {
+
+        // resources for WAV header format:
+        // [1] http://unusedino.de/ec64/technical/formats/wav.html
+        // [2] http://soundfile.sapp.org/doc/WaveFormat/
+
+        // update RIFF chunk size
+        let fileLength = data.length
+        var riffChunkSize = UInt32(fileLength - 8)
+        let riffChunkSizeRange = NSMakeRange(4, 4)
+        data.replaceBytesInRange(riffChunkSizeRange, withBytes: &riffChunkSize)
+
+        // find data subchunk
+        var subchunkID: String? = ""
+        var subchunkSize = 0
+        var offset = 12
+        let size = 4
+        while true {
+            subchunkID = dataToUTF8String(data, offset: offset, length: size)
+            subchunkSize = dataToUInt32(data, offset: offset + size)
+            if subchunkID == "data" { break }
+            offset += size + size + subchunkSize
+        }
+
+        // compute data subchunk size (excludes id and size fields)
+        var dataSubchunkSize = UInt32(data.length - offset - 8)
+
+        // update data subchunk size
+        let dataSubchunkSizeRange = NSMakeRange(offset + size, 4)
+        data.replaceBytesInRange(dataSubchunkSizeRange, withBytes: &dataSubchunkSize)
     }
 }

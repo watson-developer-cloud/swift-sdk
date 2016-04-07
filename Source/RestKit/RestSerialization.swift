71245e41685c2e00b12f0cf4f0ffcbc0bd3226e9
Glenn R. Fisher <glennrfisher@gmail.com>
2016-04-07 15:01:14 -0500

    Add support to RestSerialization to specify JSON path to decode

--- Source/RestKit/RestSerialization.swift
+++ Source/RestKit/RestSerialization.swift
@@ -18,7 +18,10 @@ extension Request {
  
      - returns: An object response serializer.
      */
-    internal static func ObjectSerializer<T: JSONDecodable>() -> ResponseSerializer<T, NSError> {
+    internal static func ObjectSerializer<T: JSONDecodable>(
+        path: [JSONPathType]? = nil)
+        -> ResponseSerializer<T, NSError>
+    {
         return ResponseSerializer { request, response, data, error in
 
             // fail if an error was already produced
@@ -36,7 +39,20 @@ extension Request {
             // serialize a `T` from the json data
             do {
                 let json = try JSON(data: data)
-                let object = try T(json: json)
+                var object: T
+                if let path = path {
+                    switch path.count {
+                    case 0: object = try json.decode()
+                    case 1: object = try json.decode(path[0])
+                    case 2: object = try json.decode(path[0], path[1])
+                    case 3: object = try json.decode(path[0], path[1], path[2])
+                    case 4: object = try json.decode(path[0], path[1], path[2], path[3])
+                    case 5: object = try json.decode(path[0], path[1], path[2], path[3], path[4])
+                    default: throw JSON.Error.KeyNotFound(key: "ExhaustedVariadicParameterEncoding")
+                    }
+                } else {
+                    object = try json.decode()
+                }
                 return .Success(object)
             } catch {
                 let failureReason = "Data could not be serialized. Failed to parse JSON response."
@@ -54,12 +70,13 @@ extension Request {
      */
     internal func responseObject<T: JSONDecodable>(
         queue queue: dispatch_queue_t? = nil,
+        path: [JSONPathType]? = nil,
         completionHandler: Response<T, NSError> -> Void)
         -> Self
     {
         return response(
             queue: queue,
-            responseSerializer: Request.ObjectSerializer(),
+            responseSerializer: Request.ObjectSerializer(path),
             completionHandler: completionHandler
         )
     }

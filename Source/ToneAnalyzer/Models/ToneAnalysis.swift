d247b68be77801be6a68e22704a79c43f238904b
Hsaylor <harrisonsaylor@gmail.com>
2016-05-12 08:11:44 -0500

    Add ToneAnalyzer to RestKit (#245)
    * Add ToneAnalyzer to MILRestKit
    
    * Remove unnecessary comments
    
    * Update README
    
    * Update class to ToneAnalyzerV3, convert to POST
    
    * Change ToneAnalyzer folder name to include V3
    
    * Update copyright headers

---
+++ Source/ToneAnalyzer/Models/ToneAnalysis.swift
@@ -0,0 +1,41 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+import Freddy
+
+extension ToneAnalyzerV3 {
+    
+    /**
+     *
+     * Main object containing the result of running Tone Analyzer on a document. 
+     * It contains both the sentence-level and document-level results.
+     */
+    public struct ToneAnalysis: JSONDecodable {
+        
+        // The tone analysis of the full document.
+        public let documentTone:ElementTone
+        
+        /// The sentence level tone analysis.
+        public let sentencesTones:[SentenceTone]
+        
+        public init(json: JSON) throws {
+            documentTone = try json.decode("document_tone", type: ElementTone.self)
+            sentencesTones = try json.arrayOf("sentences_tone", type: SentenceTone.self)
+            
+        }
+    }
+}
---
+++ Source/ToneAnalyzer/Models/ToneScore.swift
@@ -0,0 +1,40 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+import Freddy
+
+extension ToneAnalyzerV3 {
+    /**
+     *
+     * Object representing scoring of a single Tone (of any category) on our responses. It contains the
+     * Tone ID, a score, and optionally a list of evidences.
+     */
+    public struct ToneScore: JSONDecodable {
+        /// Unique ID of the tone.
+        public let id: String
+        /// The name of the tone
+        public let name: String
+        /// The score of the tone.
+        public let score: Double
+        
+        public init(json: JSON) throws {
+            id = try json.string("tone_id")
+            name = try json.string("tone_name")
+            score = try json.double("score")
+        }
+    }
+}

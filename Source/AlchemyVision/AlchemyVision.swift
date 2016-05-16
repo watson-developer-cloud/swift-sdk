a9b80f313544d2d98700db3b8de731373e963f7c
Glenn R. Fisher <glennrfisher@gmail.com>
2016-05-16 16:14:36 -0500

    Add comments and remaining endpoints to AlchemyVision

--- Source/AlchemyVision/AlchemyVision.swift
+++ Source/AlchemyVision/AlchemyVision.swift
@@ -40,7 +40,56 @@ public class AlchemyVision {
     
     // TODO: dataToError
 
-    // TODO: POST /image/ImageGetRankedImageFaceTags
+    /**
+     Perform face recognition on an uploaded image. For each face detected, the service returns
+     the estimated bounding box, gender, age, and name (if a celebrity is detected).
+ 
+     - parameter image: The image file (.jpg, .png, or .gif) on which to perform face recognition.
+     - parameter knowledgeGraph: Should additional metadata be provided for detected celebrities?
+     - parameter failure: A function executed if an error occurs.
+     - parameter success: A function executed with information about the detected faces.
+     */
+    public func getRankedImageFaceTags(
+        image image: NSURL,
+        knowledgeGraph: Bool? = nil,
+        failure: (NSError -> Void)? = nil,
+        success: [FaceTags] -> Void)
+    {
+        // construct query parameters
+        var queryParameters = [NSURLQueryItem]()
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        queryParameters.append(NSURLQueryItem(name: "imagePostMode", value: "raw"))
+        if let knowledgeGraph = knowledgeGraph {
+            if knowledgeGraph {
+                queryParameters.append(NSURLQueryItem(name: "knowledgeGraph", value: "1"))
+            } else {
+                queryParameters.append(NSURLQueryItem(name: "knowledgeGraph", value: "0"))
+            }
+        }
+        
+        // load image data
+        let data = NSData(contentsOfURL: image)
+        
+        // construct REST request
+        let request = RestRequest(
+            method: .POST,
+            url: serviceURL + "/image/ImageGetRankedImageFaceTags",
+            acceptType: "application/json",
+            contentType: "application/x-www-form-urlencoded",
+            queryParameters: queryParameters,
+            messageBody: data
+        )
+        
+        // execute REST request
+        Alamofire.request(request)
+            .responseArray { (response: Response<[FaceTags], NSError>) in
+                switch response.result {
+                case .Success(let faceTags): success(faceTags)
+                case .Failure(let error): failure?(error)
+                }
+            }
+    }
 
     /**
      Perform face recognition on the primary image at a given URL. For each face detected,
@@ -53,16 +102,16 @@ public class AlchemyVision {
      - success: A function executed with information about the detected faces.
      */
     public func getRankedImageFaceTags(
-        url: String,
+        url url: String,
         knowledgeGraph: Bool? = nil,
         failure: (NSError -> Void)? = nil,
         success: [FaceTags] -> Void)
     {
         // construct query parameters
         var queryParameters = [NSURLQueryItem]()
-        queryParameters.append(NSURLQueryItem(name: "url", value: url))
-        queryParameters.append(NSURLQueryItem(name: "apiKey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
         queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        queryParameters.append(NSURLQueryItem(name: "url", value: url))
         if let knowledgeGraph = knowledgeGraph {
             if knowledgeGraph {
                 queryParameters.append(NSURLQueryItem(name: "knowledgeGraph", value: "1"))
@@ -76,6 +125,7 @@ public class AlchemyVision {
             method: .GET,
             url: serviceURL + "/url/URLGetRankedImageFaceTags",
             acceptType: "application/json",
+            contentType: "application/x-www-form-urlencoded",
             queryParameters: queryParameters
         )
 
@@ -98,36 +148,52 @@ public class AlchemyVision {
      - parameter success: A function executed with information about the identified primary image.
      */
     public func getImage(
-        html: String,
+        html html: String,
         url: String,
         failure: (NSError -> Void)? = nil,
         success: [ImageLink] -> Void)
     {
-        // TODO: convert this function to use POST /html/HTMLGetImage instead.
-
         // construct query parameters
         var queryParameters = [NSURLQueryItem]()
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
         queryParameters.append(NSURLQueryItem(name: "html", value: html))
         queryParameters.append(NSURLQueryItem(name: "url", value: url))
-        queryParameters.append(NSURLQueryItem(name: "apiKey", value: apiKey))
-        queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
 
         // construct REST request
         let request = RestRequest(
-            method: .GET,
+            method: .POST,
             url: serviceURL + "/html/HTMLGetImage",
             acceptType: "application/json",
+            contentType: "application/x-www-form-urlencoded",
             queryParameters: queryParameters
         )
 
         // execute REST request
-        Alamofire.request(request)
-            .responseArray { (response: Response<[ImageLink], NSError>) in
-                switch response.result {
-                case .Success(let imageLinks): success(imageLinks)
-                case .Failure(let error): failure?(error)
+        Alamofire.upload(request,
+            multipartFormData: { multipartFormData in
+                if let data = html.dataUsingEncoding(NSUTF8StringEncoding) {
+                    multipartFormData.appendBodyPart(data: data, name: "html")
+                }
+            },
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .Success(let upload, _, _):
+                    upload.responseArray { (response: Response<[ImageLink], NSError>) in
+                        switch response.result {
+                        case .Success(let imageLinks): success(imageLinks)
+                        case .Failure(let error): failure?(error)
+                        }
+                    }
+                case .Failure:
+                    let failureReason = "HTML could not be encoded as form data."
+                    let userInfo = [NSLocalizedFailureReasonErrorKey: failureReason]
+                    let error = NSError(domain: self.domain, code: 0, userInfo: userInfo)
+                    failure?(error)
+                    return
                 }
             }
+        )
     }
 
     /**
@@ -138,15 +204,15 @@ public class AlchemyVision {
      - parameter success: A function executed with information about the identified primary image.
      */
     public func getImage(
-        url: String,
+        url url: String,
         failure: (NSError -> Void)? = nil,
         success: [ImageLink] -> Void)
     {
         // construct query parameters
         var queryParameters = [NSURLQueryItem]()
-        queryParameters.append(NSURLQueryItem(name: "url", value: url))
-        queryParameters.append(NSURLQueryItem(name: "apiKey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
         queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        queryParameters.append(NSURLQueryItem(name: "url", value: url))
 
         // construct REST request
         let request = RestRequest(
@@ -167,18 +233,18 @@ public class AlchemyVision {
     }
 
     // TODO: POST /image/ImageGetRankedImageKeywords
-
+    
     /**
-     Tag the primary image at a given URL.
-
-     - parameter url: The URL at which to perform image tagging.
+     Perform image tagging on an uploaded image.
+ 
+     - parameter image: The image file (.jpg, .png, or .gif) on which to perform face recognition.
      - parameter forceShowAll: Should lower confidence tags be included in the response?
-     - parameter knowledgeGraph: Should additional metadata be provided for detected celebrities?
+     - parameter knowledgeGraph: Should hierarchical metadata be provided for each tag?
      - parameter failure: A function executed if an error occurs.
      - parameter success: A function executed with the identified tags.
      */
     public func getRankedImageKeywords(
-        url: String,
+        image image: NSURL,
         forceShowAll: Bool? = nil,
         knowledgeGraph: Bool? = nil,
         failure: (NSError -> Void)? = nil,
@@ -186,9 +252,67 @@ public class AlchemyVision {
     {
         // construct query parameters
         var queryParameters = [NSURLQueryItem]()
-        queryParameters.append(NSURLQueryItem(name: "url", value: url))
-        queryParameters.append(NSURLQueryItem(name: "apiKey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
         queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        if let forceShowAll = forceShowAll {
+            if forceShowAll {
+                queryParameters.append(NSURLQueryItem(name: "forceShowAll", value: "1"))
+            } else {
+                queryParameters.append(NSURLQueryItem(name: "forceShowAll", value: "0"))
+            }
+        }
+        if let knowledgeGraph = knowledgeGraph {
+            if knowledgeGraph {
+                queryParameters.append(NSURLQueryItem(name: "knowledgeGraph", value: "1"))
+            } else {
+                queryParameters.append(NSURLQueryItem(name: "knowledgeGraph", value: "0"))
+            }
+        }
+        
+        // load image data
+        let data = NSData(contentsOfURL: image)
+        
+        // construct REST request
+        let request = RestRequest(
+            method: .POST,
+            url: serviceURL + "/image/ImageGetRankedImageKeywords",
+            acceptType: "application/json",
+            contentType: "application/x-www-form-urlencoded",
+            queryParameters: queryParameters,
+            messageBody: data
+        )
+        
+        // execute REST request
+        Alamofire.request(request)
+            .responseObject { (response: Response<ImageKeywords, NSError>) in
+                switch response.result {
+                case .Success(let imageKeywords): success(imageKeywords)
+                case .Failure(let error): failure?(error)
+                }
+        }
+    }
+
+    /**
+     Perform image tagging on the primary image at a given URL.
+
+     - parameter url: The URL at which to perform image tagging.
+     - parameter forceShowAll: Should lower confidence tags be included in the response?
+     - parameter knowledgeGraph: Should hierarchical metadata be provided for each tag?
+     - parameter failure: A function executed if an error occurs.
+     - parameter success: A function executed with the identified tags.
+     */
+    public func getRankedImageKeywords(
+        url url: String,
+        forceShowAll: Bool? = nil,
+        knowledgeGraph: Bool? = nil,
+        failure: (NSError -> Void)? = nil,
+        success: ImageKeywords -> Void)
+    {
+        // construct query parameters
+        var queryParameters = [NSURLQueryItem]()
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        queryParameters.append(NSURLQueryItem(name: "url", value: url))
         if let forceShowAll = forceShowAll {
             if forceShowAll {
                 queryParameters.append(NSURLQueryItem(name: "forceShowAll", value: "1"))
@@ -221,8 +345,47 @@ public class AlchemyVision {
                 }
             }
     }
-
-    // TODO: POST /image/ImageGetRankedImageSceneText
+    
+    /**
+     Identify text in an uploaded image.
+ 
+     - parameter image: The image file (.jpg, .png, or .gif) on which to perform text detection.
+     - parameter failure: A function executed if an error occurs.
+     - parameter success: A function executed with the detected text.
+     */
+    public func getRankedImageSceneText(
+        image image: NSURL,
+        failure: (NSError -> Void)? = nil,
+        success: [SceneText] -> Void)
+    {
+        // construct query parameters
+        var queryParameters = [NSURLQueryItem]()
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        queryParameters.append(NSURLQueryItem(name: "imagePostMode", value: "raw"))
+        
+        // load image data
+        let data = NSData(contentsOfURL: image)
+        
+        // construct REST request
+        let request = RestRequest(
+            method: .POST,
+            url: serviceURL + "/url/URLGetRankedImageKeywords",
+            acceptType: "application/json",
+            queryParameters: queryParameters,
+            contentType: "application/x-www-form-urlencoded",
+            messageBody: data
+        )
+        
+        // execute REST requeset
+        Alamofire.request(request)
+            .responseArray { (response: Response<[SceneText], NSError>) in
+                switch response.result {
+                case .Success(let sceneTexts): success(sceneTexts)
+                case .Failure(let error): failure?(error)
+                }
+        }
+    }
 
     /**
      Identify text in the primary image at a given URL.
@@ -232,15 +395,15 @@ public class AlchemyVision {
      - parameter success: A function executed with the detected text.
      */
     public func getRankedImageSceneText(
-        url: String,
+        url url: String,
         failure: (NSError -> Void)? = nil,
         success: [SceneText] -> Void)
     {
         // construct query parameters
         var queryParameters = [NSURLQueryItem]()
-        queryParameters.append(NSURLQueryItem(name: "url", value: url))
-        queryParameters.append(NSURLQueryItem(name: "apiKey", value: apiKey))
+        queryParameters.append(NSURLQueryItem(name: "apikey", value: apiKey))
         queryParameters.append(NSURLQueryItem(name: "outputMode", value: "json"))
+        queryParameters.append(NSURLQueryItem(name: "url", value: url))
 
         // construct REST request
         let request = RestRequest(

73e41c31193487fed3d4c0129ba5b12cd923046f
Harrison <hgsaylor@us.ibm.com>
2016-06-13 12:30:27 -0500

    Add document conversion service

---
+++ Source/SupportingFiles/DocumentConversionV1.h
@@ -0,0 +1,19 @@
+//
+//  DocumentConversionV1.h
+//  DocumentConversionV1
+//
+//  Created by Harrison Saylor on 6/8/16.
+//  Copyright © 2016 Glenn R. Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DocumentConversionV1.
+FOUNDATION_EXPORT double DocumentConversionV1VersionNumber;
+
+//! Project version string for DocumentConversionV1.
+FOUNDATION_EXPORT const unsigned char DocumentConversionV1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentConversionV1/PublicHeader.h>
+
+

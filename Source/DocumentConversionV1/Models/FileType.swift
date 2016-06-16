4a9d1671bad3ae0f7bb3e16588980c0e7b8f7e7d
Hsaylor <harrisonsaylor@gmail.com>
2016-06-16 17:32:44 -0500

    Add document conversion (#323)
    * Add document conversion service
    
    * Organize project file folder structure
    
    * Clean up comments
    
    * Add init parameter for serviceURL
    
    Also makes some minor style/spacing changes.
    
    * Add simple config file creation function, update tests and version
    
    * Remove unused article
    
    * Add copyright header to .h file

---
+++ Source/DocumentConversionV1/Models/FileType.swift
@@ -0,0 +1,27 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+
+/**
+ Enum for supported file types
+ */
+public enum FileType: String {
+    case HTML = "text/html"
+    case XML = "text/xhtml+xml"
+    case PDF = "application/pdf"
+    case MSWord = "application/msword"
+}

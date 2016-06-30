4b10dd11a86faa25a9b791c08a0a84248f666d6a
Glenn R. Fisher <glennrfisher@gmail.com>
2016-06-30 17:34:46 -0500

    Update user agent version

--- Source/AlchemyLanguageV1/AlchemyLanguage.swift
+++ Source/AlchemyLanguageV1/AlchemyLanguage.swift
@@ -30,7 +30,7 @@ public class AlchemyLanguage {
     
     private let serviceUrl = "https://gateway-a.watsonplatform.net/calls"
     private let errorDomain = "com.watsonplatform.alchemyLanguage"
-    private let userAgent = buildUserAgent("watson-apis-ios-sdk/0.4.0 AlchemyLanguageV1")
+    private let userAgent = buildUserAgent("watson-apis-ios-sdk/0.4.1 AlchemyLanguageV1")
  
     private let unreservedCharacters = NSCharacterSet(charactersInString: "abcdefghijklmnopqrstuvwxyz" +
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
--- Source/RelationshipExtractionV1Beta/RelationshipExtraction.swift
+++ Source/RelationshipExtractionV1Beta/RelationshipExtraction.swift
@@ -31,7 +31,7 @@ public class RelationshipExtraction {
     private let username: String
     private let password: String
     private let serviceURL: String
-    private let userAgent = buildUserAgent("watson-apis-ios-sdk/0.4.0 RelationshipExtractionV1Beta")
+    private let userAgent = buildUserAgent("watson-apis-ios-sdk/0.4.1 RelationshipExtractionV1Beta")
     private let domain = "com.ibm.watson.developer-cloud.RelationshipExtractionV1Beta"
     
     /**

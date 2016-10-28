7d1db0da624e291f402261ed9afced2b502f4e22
Sarah Chen <sarahchen22@gmail.com>
2016-10-28 10:58:47 -0500

    updating linuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,22 @@
 import XCTest
 //@testable import WatsonDeveloperCloudTests
 @testable import AlchemyDataNewsV1Tests
+@testable import AlchemyLanguageV1
+@testable import AlchemyVisionV1
+@testable import ConversationV1
+@testable import DialogV1
+@testable import DocumentConversionV1
+@testable import LanguageTranslatorV2
+@testable import NaturalLanguageClassifierV1
 
 XCTMain([
      //testCase(WatsonDeveloperCloudTests.allTests),
-    testCase(AlchemyDataNewsTests.allTests)
-])
+    testCase(AlchemyDataNewsTests.allTests),
+    testCase(AlchemyLanguageTests.allTests),
+    testCase(AlchemyVisionTests.allTests),
+    testCase(ConversationTests.allTests),
+    testCase(DialogTests.allTests),
+    testCase(DocumentConversionTests.allTests),
+    testCase(LanguageTranslatorTests.allTests),
+    testCase(NaturalLanguageClassifierTests.allTests)
+    ])

9b70f1eba6c7e00c001a33051aeb147fb73623f0
Sarah Chen <sarahchen22@gmail.com>
2016-11-04 12:05:03 -0500

    Add test for smart formatting

--- Source/SpeechToTextV1/Tests/SpeechToTextTests.swift
+++ Source/SpeechToTextV1/Tests/SpeechToTextTests.swift
@@ -277,6 +277,57 @@ class SpeechToTextTests: XCTestCase {
         }
         waitForExpectations()
     }
+    
+    // MARK: - Transcribe Data with Smart Formatting
+    
+    func testTranscribeStockAnnouncementCustomWAV() {
+        transcribeDataCustomForNumbers("StockAnnouncement", withExtension: "wav", format: .WAV)
+    }
+    
+    func transcribeDataCustomForNumbers(
+        filename: String,
+        withExtension: String,
+        format: AudioMediaType)
+    {
+        let description = "Transcribe an audio file with smart formatting."
+        let expectation = expectationWithDescription(description)
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        guard let file = bundle.URLForResource(filename, withExtension: withExtension) else {
+            XCTFail("Unable to locate \(filename).\(withExtension).")
+            return
+        }
+        
+        guard let audio = NSData(contentsOfURL: file) else {
+            XCTFail("Unable to read \(filename).\(withExtension).")
+            return
+        }
+        
+        var settings = RecognitionSettings(contentType: format)
+        settings.continuous = true
+        settings.inactivityTimeout = 1
+        settings.keywords = ["stock"]
+        settings.keywordsThreshold = 0.75
+        settings.maxAlternatives = 3
+        settings.interimResults = true
+        settings.wordAlternativesThreshold = 0.25
+        settings.wordConfidence = true
+        settings.timestamps = true
+        settings.filterProfanity = false
+        settings.smartFormatting = true
+        
+        speechToText.recognize(audio, settings: settings, model: "en-US_BroadbandModel", learningOptOut: true, failure: failWithError) { results in
+            self.validateSTTResults(results.results, settings: settings)
+            if results.results.last?.final == true {
+                let transcript = results.results.last?.alternatives.last?.transcript
+                XCTAssertNotNil(transcript)
+                XCTAssertGreaterThan(transcript!.characters.count, 0)
+                XCTAssertTrue(transcript!.containsString("$152.37"))
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations()
+    }
 
     // MARK: - Transcribe Streaming
 

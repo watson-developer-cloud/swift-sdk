2fcce1542458d6c9a8d0029147249b8d43c17fb4
Stephanie Cheng <sccheng@us.ibm.com>
2016-12-06 16:24:25 -0600

    Added initializer and some default variables to Discovery

--- Source/DiscoveryV1/Discovery.swift
+++ Source/DiscoveryV1/Discovery.swift
@@ -17,6 +17,55 @@
 import Foundation
 import RestKit
 
+/**
+ The IBM Watson Discovery service uses data analysis combined with cognitive intuition to take your
+ unstructured data and enrich it so you can query it for the information you need.
+ */
 public class Discovery {
+
+    /// The base URL to use when contacting the service.
+    public var serviceURL = "https://gateway.watsonplatform.net/discovery-experimental/api/v1"
+    
+    /// The default HTTP headers for all requests to the service.
+    public var defaultHeaders = [String: String]()
+    
+    private let credentials: Credentials
+    private let domain = "com.ibm.watson.developer-cloud.DiscoveryV1"
+    private let version: String
+    
+    /**
+     Create a `Discovery` object.
+     
+     - parameter username: The username used to authenticate with the service.
+     - parameter password: The password used to authenticate with the service.
+     - parameter version: The release date of the version of the API to use. Specify the date
+            in "YYYY-MM-DD" format.
+     */
+    public init(username: String, password: String, version: String) {
+        self.credentials = Credentials.basicAuthentication(username: username, password: password)
+        self.version = version
+    }
+    
+    // MARK: - Environments
+    
+    /**
+     Create an environment for this service instance.
+     
+     For the experimental release, the size of the environment is fixed at 2GB
+     available disk space, and 1GB RAM.
+     
+     - parameter name: The name of the new environment.
+     - parameter description: The description of the new environment.
+     - parameter failure: A function executed if an error occurs.
+     - parameter success: A function executed with details of the newly created environment.
+     */
+    public func createEnvironment(
+        withName name: String,
+        withDescription description: String,
+        failure: ((Error) -> Void)? = nil,
+        success: @escaping ([Environment]) -> Void)
+    {
+        // construct body
+    }
 
 }

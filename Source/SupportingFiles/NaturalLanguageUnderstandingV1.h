e6cb6f31386bee4c2ccf32631cd69881bc2b032c
Stephanie Cheng <sccheng@us.ibm.com>
2017-01-24 16:51:10 -0600

    Added target for NLU

---
+++ Source/SupportingFiles/NaturalLanguageUnderstandingV1.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NaturalLanguageUnderstandingV1.
+FOUNDATION_EXPORT double NaturalLanguageUnderstandingV1VersionNumber;
+
+//! Project version string for NaturalLanguageUnderstandingV1.
+FOUNDATION_EXPORT const unsigned char NaturalLanguageUnderstandingV1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NaturalLanguageUnderstandingV1/PublicHeader.h>
+
+

a26e36ff6d5ac795b84053f21a69963ea1b9d6b4
Sarah Chen <sarahchen22@gmail.com>
2017-02-02 13:57:21 -0600

    Commit to save progress. Beginning to extract packets.

--- Source/TextToSpeechV1/TextToSpeechDecoder.swift
+++ Source/TextToSpeechV1/TextToSpeechDecoder.swift
@@ -23,22 +23,32 @@ internal class TextToSpeechDecoder {
     
     private var audioData: Data
     
-//    private var buffer: oggpack_buffer
+    //    private var buffer: oggpack_buffer
     private var page: ogg_page
     private var streamState: ogg_stream_state    // state of ogg stream?
     private var packet: ogg_packet          // packet within a stream passing information
     private var syncState: ogg_sync_state        // state of ogg sync
-    private var beginStream: true
+    private var beginStream = true
+    private var pageGranulePosition: Int64 = 0     // Positiion of the packet data at page
+    private var hasOpusStream = false
+    private var hasTagsPacket = false
     
-    convenience init(data: Data) {
-        self.audioData = data
+    convenience init(data: Data) throws {
+        
+        try self.init(
+            audioData: data
+        )
         
     }
     
-    init() throws {
+    init(audioData: Data) throws {
         // set properties
+        streamState = ogg_stream_state()
+        syncState = ogg_sync_state()
+        page = ogg_page()
+        packet = ogg_packet()
         
-        // initialize ogg sync state 
+        // initialize ogg sync state
         ogg_sync_init(&syncState)
         var processedByteCount = 0 // guarantee data is pulled from stream.
         var audioDataSize = audioData.count
@@ -52,7 +62,7 @@ internal class TextToSpeechDecoder {
         } else {
             bufferSize = audioData.count - processedByteCount
         }
-//        let range = Range(uncheckedBounds: (lower: processedByteCount, upper: bufferSize))
+        //        let range = Range(uncheckedBounds: (lower: processedByteCount, upper: bufferSize))
         bufferData = ogg_sync_buffer(&syncState, bufferSize)
         
         for index in processedByteCount..<bufferSize+processedByteCount {
@@ -74,54 +84,52 @@ internal class TextToSpeechDecoder {
             if (ogg_page_serialno(&page) != Int32(streamState.serialno)) {
                 ogg_stream_reset_serialno(&streamState, ogg_page_serialno(&page))
             }
+            
+            /// Add page to the stream.
+            ogg_stream_pagein(&streamState, &page)
+            
+            /// Check granularity position?
+            pageGranulePosition = ogg_page_granulepos(&page)
+            
+            /// While there's a packet, extract.
+            if extractPacket(&streamState, &packet) {
+                print ("yay")
+            }
+//            while (ogg_stream_packetout(&streamState, &packet) == 1) {
+//                /// Check for initial stream header
+//                if (packet.bytes >= 8 && packet.b_o_s == 1) {
+//                    
+//                }
+//            }
+            
+            
+            
+            // initialize ogg stream state to allocate memory to decode. Returns 0 if successful.
+            //        let status = ogg_stream_init(&streamState, serial)
+            //        guard status == 0 else {
+            //            throw OpusError.internalError
+            //        }
+            // build buffer.
+            
         }
-        
-        // initialize ogg stream state to allocate memory to decode. Returns 0 if successful.
-        let status = ogg_stream_init(&streamState, serial)
-        guard status == 0 else {
-            throw OpusError.internalError
-        }
-        // build buffer.
         
     }
     
-}
 
-// MARK: - OpusError
-internal enum OpusError: Error {
-    case ok
-    case badArgument
-    case bufferTooSmall
-    case internalError
-    case invalidPacket
-    case unimplemented
-    case invalidState
-    case allocationFailure
-    
-    var rawValue: Int32 {
-        switch self {
-        case .ok: return OPUS_OK
-        case .badArgument: return OPUS_BAD_ARG
-        case .bufferTooSmall: return OPUS_BUFFER_TOO_SMALL
-        case .internalError: return OPUS_INTERNAL_ERROR
-        case .invalidPacket: return OPUS_INVALID_PACKET
-        case .unimplemented: return OPUS_UNIMPLEMENTED
-        case .invalidState: return OPUS_INVALID_STATE
-        case .allocationFailure: return OPUS_ALLOC_FAIL
+    private func extractPacket(_ streamState: inout ogg_stream_state, _ packet: inout ogg_packet) -> Bool {
+        while (ogg_stream_packetout(&streamState, &packet) == 1) {
+            /// Check for initial stream header
+            if (packet.bytes >= 8 && packet.b_o_s == 1 && (memcmp(packet.packet, "OpusHead", 8) == 0)) {
+                /// Check if there's another opus head to see if stream is chained without EOS.
+                if (hasOpusStream && hasTagsPacket) {
+                    hasOpusStream = false
+                    if
+                }
+            }
+            return false
         }
+        return false
     }
     
-    init?(rawValue: Int32) {
-        switch rawValue {
-        case OPUS_OK: self = .ok
-        case OPUS_BAD_ARG: self = .badArgument
-        case OPUS_BUFFER_TOO_SMALL: self = .bufferTooSmall
-        case OPUS_INTERNAL_ERROR: self = .internalError
-        case OPUS_INVALID_PACKET: self = .invalidPacket
-        case OPUS_UNIMPLEMENTED: self = .unimplemented
-        case OPUS_INVALID_STATE: self = .invalidState
-        case OPUS_ALLOC_FAIL: self = .allocationFailure
-        default: return nil
-        }
-    }
+    private func processHeader(_ packet inout ogg_packet, _ rate opus_int32 *rate, int *mapping_family, int *channels, int *preskip, float *gain, float manual_gain, int *streams, int wav_format)
 }

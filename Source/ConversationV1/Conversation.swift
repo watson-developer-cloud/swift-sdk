0a178a342d51679035fc230ce6679499cee1ca41
Mike Kistler <mkistler@us.ibm.com>
2017-07-18 21:39:17 -0500

    Set error message in error with NSLocalizedDescriptionKey

--- Source/ConversationV1/Conversation.swift
+++ Source/ConversationV1/Conversation.swift
@@ -78,7 +78,7 @@ public class Conversation {
             let json = try JSON(data: data)
             let code = response?.statusCode ?? 400
             let message = try json.getString(at: "error")
-            let userInfo = [NSLocalizedFailureReasonErrorKey: message]
+            let userInfo = [NSLocalizedDescriptionKey: message]
             return NSError(domain: domain, code: code, userInfo: userInfo)
         } catch {
             return nil

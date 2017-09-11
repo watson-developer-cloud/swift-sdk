a381dec61c6ebd83a655d9651fa1dd212f3f20ef
Glenn R. Fisher <glennrfisher@gmail.com>
2017-09-11 08:31:22 -0500

    Add include for <sys/types.h>

--- Source/SupportingFiles/Dependencies/ogg/os_types.h
+++ Source/SupportingFiles/Dependencies/ogg/os_types.h
@@ -69,6 +69,7 @@
 #elif (defined(__APPLE__) && defined(__MACH__)) /* MacOS X Framework build */
 
 // #  include <inttypes.h>
+#  include <sys/types.h>
    typedef int16_t ogg_int16_t;
    typedef uint16_t ogg_uint16_t;
    typedef int32_t ogg_int32_t;

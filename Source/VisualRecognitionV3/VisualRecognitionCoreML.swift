7eedf2262e367039190dc2d9446c1a9745534e3d
Devin Conley <devinaconley@ibm.com>
2017-11-10 12:23:19 -0600

    moved coreml functions to extension of VR class

--- Source/VisualRecognitionV3/VisualRecognitionCoreML.swift
+++ Source/VisualRecognitionV3/VisualRecognitionCoreML.swift
@@ -17,6 +17,7 @@
 import Foundation
 import CoreML
 import Vision
+import RestKit
 
 
 // Model abstraction class
@@ -100,5 +101,119 @@ public class VisualRecognitionCoreMLModel {
 
 @available(iOS 11.0, *)
 extension VisualRecognition {
-    // TODO -- proper extension here
+    
+    /**
+     Classify an image with CoreML, given a passed model. On failure or low confidence, fallback to Watson VR cloud service
+     
+     - parameter image: The image as NSData
+     - parameter model: CoreML model
+     - parameter localThreshold: minimum local score to return results immediately
+     - parameter owners: A list of the classifiers to run. Acceptable values are "IBM" and "me".
+     - parameter classifierIDs: A list of the classifier ids to use. "default" is the id of the
+     built-in classifier.
+     - parameter threshold: The minimum score a class must have to be displayed in the response.
+     - parameter language: The language of the output class names. Can be "en" (English), "es"
+     (Spanish), "ar" (Arabic), or "ja" (Japanese). Classes for which no translation is available
+     are omitted.
+     - parameter failure: A function executed if an error occurs.
+     - parameter success: A function executed with the image classifications.
+     */
+    public func classify(
+        image: NSData,
+        model: VNCoreMLModel,
+        localThreshold: Double? = nil,
+        owners: [String]? = nil,
+        classifierIDs: [String]? = nil,
+        threshold: Double? = nil,
+        language: String? = nil,
+        failure: ((Error) -> Void)? = nil,
+        success: @escaping (ClassifiedImages) -> Void)
+    {
+        // short-circuit coreml if local thresh is 1.0
+        if localThreshold == 1.0 {
+            self.classify(image: image, owners: owners, classifierIDs:classifierIDs, threshold:threshold, language:language, failure:failure, success: success)
+            return
+        }
+        
+        print ( "trying local classification on CoreML..." )
+        
+        // setup request
+        let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+            // define coreml callback
+            guard let results = request.results else {
+                print( "Unable to classify image.\n\(error!.localizedDescription)" )
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            var classifications = results as! [VNClassificationObservation]
+            
+            if let thresh = localThreshold {
+                classifications = classifications.filter({ $0.confidence > Float(thresh) })
+            }
+            
+            if classifications.isEmpty {
+                print( "Nothing recognized." )
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(20)
+                
+                // convert results to sdk vision models
+                var scores = [[String: Any]]()
+                for c in topClassifications {
+                    let temp: [String: Any] = [
+                        "class" : c.identifier,
+                        "score" : Double( c.confidence )
+                    ]
+                    scores.append( temp )
+                }
+                
+                let bodyClassifier: [String: Any] = [
+                    "name": "coreml",
+                    "classifier_id": "",
+                    "classes" : scores
+                ]
+                
+                let bodyIm: [String: Any] = [
+                    "source_url" : "",
+                    "resolved_url" : "",
+                    "image": "",
+                    "error": "",
+                    "classifiers": [bodyClassifier]
+                ]
+                
+                let body: [String: Any] = [
+                    "images" : [bodyIm],
+                    "warning" :[]
+                ]
+                do {
+                    let converted = try ClassifiedImages( json: JSON(dictionary: body) )
+                    success( converted )
+                    return
+                } catch {
+                    print( error )
+                }
+            }
+            
+            // hit standard VR service as fallback
+            self.classify(image: image, owners: owners, classifierIDs:classifierIDs, threshold:threshold, language:language, failure:failure, success: success)
+            
+        })
+        request.imageCropAndScaleOption = .scaleFill // This seems wrong, but yields results in line with vision demo
+        
+        // do request with handler in background
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(data: image as Data)
+            do {
+                try handler.perform([request])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
 }

203e434820aaebaa49abf63fa1b5e31a130dacb8
Ajiemar Santiago <ajiemar@gmail.com>
2018-01-22 13:25:56 -0600

    parse local model updated date from created & retrained

--- Source/VisualRecognitionV3/VisualRecognition+CoreML.swift
+++ Source/VisualRecognitionV3/VisualRecognition+CoreML.swift
@@ -57,10 +57,10 @@ extension VisualRecognition {
             return
         }
 
-        // parse model's `updated` date
+        // parse the date on which the local model was last updated
         let description = model.modelDescription
         let metadata = description.metadata[MLModelMetadataKey.creatorDefinedKey] as? [String: String] ?? [:]
-        guard let updated = metadata["updated"], let modelDate = dateFormatter.date(from: updated) else {
+        guard let updated = metadata["retrained"] ?? metadata["created"], let modelDate = dateFormatter.date(from: updated) else {
             downloadClassifier(classifierID: classifierID, failure: failure, success: success)
             return
         }

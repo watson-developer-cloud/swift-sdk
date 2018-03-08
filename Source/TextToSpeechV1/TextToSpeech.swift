2ab1b6926be68aca7d7a2211ffa39b7c7c99255a
Glenn R. Fisher <glennrfisher@gmail.com>
2018-03-08 14:41:33 -0600

    Fix indentation

--- Source/TextToSpeechV1/TextToSpeech.swift
+++ Source/TextToSpeechV1/TextToSpeech.swift
@@ -17,7 +17,7 @@
 import Foundation
 
 /**
-  ### Service Overview
+ ### Service Overview
  The IBM Text to Speech service provides a Representational State Transfer (REST) Application Programming Interface
  (API) that uses IBM's speech-synthesis capabilities to synthesize text into natural-sounding speech in a variety of
  languages, dialects, and voices. The service currently synthesizes text from US English, UK English, French, German,
@@ -61,7 +61,7 @@ import Foundation
 
   &lt;phoneme alphabet="ibm" ph="1gAstroEntxrYFXs"&gt;&lt;/phoneme&gt;
 
-  For more information about customization and about sounds-like and phonetic translations, see [Understanding
+ For more information about customization and about sounds-like and phonetic translations, see [Understanding
  customization](https://console.bluemix.net/docs/services/text-to-speech/custom-intro.html).
  * **GUIDs:** The pronunciation and customization methods accept or return a Globally Unique Identifier (GUID). For
  example, customization IDs (specified with the `customization_id` parameter) and service credentials are GUIDs. GUIDs
@@ -85,7 +85,7 @@ import Foundation
  [Controlling request logging for Watson
  services](https://console.bluemix.net/docs/services/watson/getting-started-logging.html).
 
-  The service does not log data (words and translations) that are used to build custom language models; your training
+ The service does not log data (words and translations) that are used to build custom language models; your training
  data is never used to improve the service's base models. The service does log data when a custom model is used with a
  synthesize request; you must set the `X-Watson-Learning-Opt-Out` request header to prevent logging for recognition
  requests. For more information, see [Request logging and data
@@ -157,7 +157,7 @@ public class TextToSpeech {
     /**
      Retrieves a specific voice available for speech synthesis.
 
-          Lists information about the voice specified with the `voice` path parameter. Specify the `customization_id` query
+     Lists information about the voice specified with the `voice` path parameter. Specify the `customization_id` query
      parameter to obtain information for that custom voice model of the specified voice. Use the `GET /v1/voices` method
      to see a list of all available voices.
 
@@ -208,7 +208,7 @@ public class TextToSpeech {
     /**
      Retrieves all voices available for speech synthesis.
 
-          Lists information about all available voices. To see information about a specific voice, use the `GET
+     Lists information about all available voices. To see information about a specific voice, use the `GET
      /v1/voices/{voice}` method.
 
      - parameter failure: A function executed if an error occurs.
@@ -242,7 +242,7 @@ public class TextToSpeech {
     /**
      Streaming speech synthesis of the text in the body parameter.
 
-          Synthesizes text to spoken audio, returning the synthesized audio stream as an array of bytes. Identical to the
+     Synthesizes text to spoken audio, returning the synthesized audio stream as an array of bytes. Identical to the
      `GET` method but passes longer text in the body of the request, not with the URL. Text size is limited to 5 KB.
      (For the `audio/l16` format, you can optionally specify `endianness=big-endian` or `endianness=little-endian`; the
      default is little endian.)   If a request includes invalid query parameters, the service returns a `Warnings`
@@ -341,7 +341,7 @@ public class TextToSpeech {
     /**
      Gets the pronunciation for a word.
 
-          Returns the phonetic pronunciation for the word specified by the `text` parameter. You can request the
+     Returns the phonetic pronunciation for the word specified by the `text` parameter. You can request the
      pronunciation for a specific format. You can also request the pronunciation for a specific voice to see the default
      translation for the language of that voice or for a specific custom voice model to see the translation for that
      voice model.   **Note:** This method is currently a beta release.
@@ -401,7 +401,7 @@ public class TextToSpeech {
     /**
      Creates a new custom voice model.
 
-          Creates a new empty custom voice model. The model is owned by the instance of the service whose credentials are
+     Creates a new empty custom voice model. The model is owned by the instance of the service whose credentials are
      used to create it.   **Note:** This method is currently a beta release.
 
      - parameter name: The name of the new custom voice model.
@@ -449,7 +449,7 @@ public class TextToSpeech {
     /**
      Deletes a custom voice model.
 
-          Deletes the custom voice model with the specified `customization_id`. You must use credentials for the instance of
+     Deletes the custom voice model with the specified `customization_id`. You must use credentials for the instance of
      the service that owns a model to delete it.   **Note:** This method is currently a beta release.
 
      - parameter customizationID: The GUID of the custom voice model that is to be deleted. You must make the request with service credentials created for the instance of the service that owns the custom model.
@@ -489,7 +489,7 @@ public class TextToSpeech {
     /**
      Queries the contents of a custom voice model.
 
-          Lists all information about the custom voice model with the specified `customization_id`. In addition to metadata
+     Lists all information about the custom voice model with the specified `customization_id`. In addition to metadata
      such as the name and description of the voice model, the output includes the words in the model and their
      translations as defined in the model. To see just the metadata for a voice model, use the `GET /v1/customizations`
      method. You must use credentials for the instance of the service that owns a model to list information about it.
@@ -533,7 +533,7 @@ public class TextToSpeech {
     /**
      Lists all available custom voice models for a language or for all languages.
 
-          Lists metadata such as the name and description for the custom voice models that you own. Use the `language` query
+     Lists metadata such as the name and description for the custom voice models that you own. Use the `language` query
      parameter to list the voice models that you own for the specified language only. Omit the parameter to see all
      voice models that you own for all languages. To see the words in addition to the metadata for a specific voice
      model, use the `GET /v1/customizations/{customization_id}` method. You must use credentials for the instance of the
@@ -579,7 +579,7 @@ public class TextToSpeech {
     /**
      Updates information and words for a custom voice model.
 
-          Updates information for the custom voice model with the specified `customization_id`. You can update the metadata
+     Updates information for the custom voice model with the specified `customization_id`. You can update the metadata
      such as the name and description of the voice model. You can also update the words in the model and their
      translations. Adding a new translation for a word that already exists in a custom model overwrites the word's
      existing translation. A custom model can contain no more than 20,000 entries. You must use credentials for the
@@ -636,7 +636,7 @@ public class TextToSpeech {
     /**
      Adds a word to a custom voice model.
 
-          Adds a single word and its translation to the custom voice model with the specified `customization_id`. Adding a
+     Adds a single word and its translation to the custom voice model with the specified `customization_id`. Adding a
      new translation for a word that already exists in a custom model overwrites the word's existing translation. A
      custom model can contain no more than 20,000 entries. You must use credentials for the instance of the service that
      owns a model to add a word to it.   **Note:** This method is currently a beta release.
@@ -692,7 +692,7 @@ public class TextToSpeech {
     /**
      Adds one or more words to a custom voice model.
 
-          Adds one or more words and their translations to the custom voice model with the specified `customization_id`.
+     Adds one or more words and their translations to the custom voice model with the specified `customization_id`.
      Adding a new translation for a word that already exists in a custom model overwrites the word's existing
      translation. A custom model can contain no more than 20,000 entries. You must use credentials for the instance of
      the service that owns a model to add words to it.   **Note:** This method is currently a beta release.
@@ -744,7 +744,7 @@ public class TextToSpeech {
     /**
      Deletes a word from a custom voice model.
 
-          Deletes a single word from the custom voice model with the specified `customization_id`. You must use credentials
+     Deletes a single word from the custom voice model with the specified `customization_id`. You must use credentials
      for the instance of the service that owns a model to delete it.   **Note:** This method is currently a beta
      release.
 
@@ -787,7 +787,7 @@ public class TextToSpeech {
     /**
      Queries details about a word in a custom voice model.
 
-          Returns the translation for a single word from the custom model with the specified `customization_id`. The output
+     Returns the translation for a single word from the custom model with the specified `customization_id`. The output
      shows the translation as it is defined in the model. You must use credentials for the instance of the service that
      owns a model to query information about its words.   **Note:** This method is currently a beta release.
 
@@ -831,7 +831,7 @@ public class TextToSpeech {
     /**
      Queries details about the words in a custom voice model.
 
-          Lists all of the words and their translations for the custom voice model with the specified `customization_id`. The
+     Lists all of the words and their translations for the custom voice model with the specified `customization_id`. The
      output shows the translations as they are defined in the model. You must use credentials for the instance of the
      service that owns a model to query information about its words.   **Note:** This method is currently a beta
      release.

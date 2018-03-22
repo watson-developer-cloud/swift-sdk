7935974d1c0edf117a898c9f3caa4abc35bbe609
Glenn R. Fisher <glennrfisher@gmail.com>
2018-03-22 10:30:38 -0500

    Update sdkVersion for v0.23.1

--- Source/RestKit/RestRequest.swift
+++ Source/RestKit/RestRequest.swift
@@ -20,7 +20,7 @@ internal struct RestRequest {
 
     internal static let userAgent: String = {
         let sdk = "watson-apis-swift-sdk"
-        let sdkVersion = "0.23.0"
+        let sdkVersion = "0.23.1"
 
         let operatingSystem: String = {
             #if os(iOS)

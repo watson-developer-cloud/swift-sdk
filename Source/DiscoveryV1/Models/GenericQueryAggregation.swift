565342cba287c8d838e77d503eec11961e0a6999
Glenn R. Fisher <glennrfisher@gmail.com>
2018-03-26 16:16:49 -0500

    Rename QueryAggregation to GenericQueryAggregation

---
+++ Source/DiscoveryV1/Models/GenericQueryAggregation.swift
@@ -0,0 +1,49 @@
+/**
+ * Copyright IBM Corporation 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import Foundation
+
+/** An aggregation produced by the Discovery service to analyze the input provided. */
+public struct GenericQueryAggregation: Decodable {
+
+    /// The type of aggregation command used. For example: term, filter, max, min, etc.
+    public var type: String?
+
+    /// The field where the aggregation is located in the document.
+    public var field: String?
+
+    public var results: [AggregationResult]?
+
+    /// The match the aggregated results queried for.
+    public var match: String?
+
+    /// Number of matching results.
+    public var matchingResults: Int?
+
+    /// Aggregations returned by the Discovery service.
+    public var aggregations: [QueryAggregation]?
+
+    // Map each property name to the key that shall be used for encoding/decoding.
+    private enum CodingKeys: String, CodingKey {
+        case type = "type"
+        case field = "field"
+        case results = "results"
+        case match = "match"
+        case matchingResults = "matching_results"
+        case aggregations = "aggregations"
+    }
+
+}

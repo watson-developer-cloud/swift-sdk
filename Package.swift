9643d8ee1e3dfcd88c23f4b05ca3792dda5cb03a
Anthony Oliveri <oliveri@us.ibm.com>
2018-09-10 15:16:56 -0500

    Temporarily switch to RestKit side branch "swift-sdk-1.0"

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         .library(name: "VisualRecognitionV3", targets: ["VisualRecognitionV3"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/watson-developer-cloud/restkit.git", from: "1.0.0")
+        .package(url: "https://github.com/watson-developer-cloud/restkit.git", .branch("swift-sdk-1.0"))
     ],
     targets: [
         .target(name: "AssistantV1", dependencies: ["RestKit"]),

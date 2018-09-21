25f67af5e604f98935ad67d32b31ac28d6252ebd
Anthony Oliveri <oliveri@us.ibm.com>
2018-09-21 09:33:06 -0500

    Fix Linux unit tests for AssistantV2
    The generator should have added the "Content-Type: application/json" header to createSession(), but didn't for some reason

--- Source/AssistantV2/Assistant.swift
+++ Source/AssistantV2/Assistant.swift
@@ -130,6 +130,7 @@ public class Assistant {
             headerParameters.merge(headers) { (_, new) in new }
         }
         headerParameters["Accept"] = "application/json"
+        headerParameters["Content-Type"] = "application/json"
 
         // construct query parameters
         var queryParameters = [URLQueryItem]()

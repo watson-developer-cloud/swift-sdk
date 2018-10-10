7420b17ee803df1a9b8d037e1c20eb56d330e4d7
Anthony Oliveri <oliveri@us.ibm.com>
2018-10-10 12:04:10 -0500

    test: Update version date for all tests

--- Tests/VisualRecognitionV3Tests/VisualRecognition+CoreMLTests.swift
+++ Tests/VisualRecognitionV3Tests/VisualRecognition+CoreMLTests.swift
@@ -36,7 +36,7 @@ class VisualRecognitionCoreMLTests: XCTestCase {
 
     /** Instantiate Visual Recognition */
     func instantiateVisualRecognition() {
-        let version = "2018-09-14"
+        let version = "2018-10-10"
         if let apiKey = WatsonCredentials.VisualRecognitionAPIKey {
             visualRecognition = VisualRecognition(version: version, apiKey: apiKey)
         } else {

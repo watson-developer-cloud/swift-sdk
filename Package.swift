3bac33506285507187bcca0f4fe1d74597e9299b
Anthony Oliveri <oliveri@us.ibm.com>
2018-10-18 14:21:28 -0500

    Fix Linux build
    Updated RestKit dependency to >1.3.0

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         .library(name: "VisualRecognitionV3", targets: ["VisualRecognitionV3"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/watson-developer-cloud/restkit.git", from: "1.2.0")
+        .package(url: "https://github.com/watson-developer-cloud/restkit.git", from: "1.3.0")
     ],
     targets: [
         .target(name: "AssistantV1", dependencies: ["RestKit"]),

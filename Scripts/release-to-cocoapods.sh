9bf16263629d9d22a38f0ab7c4cfa2fbae824b50
Anthony Oliveri <oliveri@us.ibm.com>
2018-10-19 11:40:34 -0500

    ci(Cocoapods): Re-add all services to Cocoapods deployment
    Successfully deployed the remainder of the services for version 0.36.0, so the all services are
    added back to the script

--- Scripts/release-to-cocoapods.sh
+++ Scripts/release-to-cocoapods.sh
@@ -5,14 +5,14 @@
 set -e
 
 declare -a allPods=(
-  # "IBMWatsonAssistantV1.podspec"
-  # "IBMWatsonAssistantV2.podspec"
-  # "IBMWatsonConversationV1.podspec"
-  # "IBMWatsonDiscoveryV1.podspec"
-  # "IBMWatsonLanguageTranslatorV3.podspec"
-  # "IBMWatsonNaturalLanguageClassifierV1.podspec"
-  # "IBMWatsonNaturalLanguageUnderstandingV1.podspec"
-  # "IBMWatsonPersonalityInsightsV3.podspec"
+  "IBMWatsonAssistantV1.podspec"
+  "IBMWatsonAssistantV2.podspec"
+  "IBMWatsonConversationV1.podspec"
+  "IBMWatsonDiscoveryV1.podspec"
+  "IBMWatsonLanguageTranslatorV3.podspec"
+  "IBMWatsonNaturalLanguageClassifierV1.podspec"
+  "IBMWatsonNaturalLanguageUnderstandingV1.podspec"
+  "IBMWatsonPersonalityInsightsV3.podspec"
   "IBMWatsonSpeechToTextV1.podspec"
   "IBMWatsonTextToSpeechV1.podspec"
   "IBMWatsonToneAnalyzerV3.podspec"

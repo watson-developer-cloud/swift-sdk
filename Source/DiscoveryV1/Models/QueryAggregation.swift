67cf59450fcdc41b41e9a1ab906f6ad3a17f8162
Mike Kistler <mkistler@us.ibm.com>
2018-11-06 12:51:01 -0600

    Project changes for codable models

--- Source/DiscoveryV1/Models/QueryAggregation.swift
+++ Source/DiscoveryV1/Models/QueryAggregation.swift
@@ -17,7 +17,7 @@
 import Foundation
 
 /** An aggregation produced by the Discovery service to analyze the input provided. */
-public enum QueryAggregation: Decodable {
+public enum QueryAggregation: Codable {
 
     // reference: https://console.bluemix.net/docs/services/discovery/query-reference.html#aggregations
 
@@ -59,6 +59,47 @@ public enum QueryAggregation: Decodable {
         case "average": self = .average(try Calculation(from: decoder))
         case "sum": self = .sum(try Calculation(from: decoder))
         default: self = .generic(try GenericQueryAggregation(from: decoder))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .term(let term):
+            try container.encode("term", forKey: .type)
+            try term.encode(to: encoder)
+        case .filter(let filter):
+            try container.encode("filter", forKey: .type)
+            try filter.encode(to: encoder)
+        case .nested(let nested):
+            try container.encode("nested", forKey: .type)
+            try nested.encode(to: encoder)
+        case .histogram(let histogram):
+            try container.encode("histogram", forKey: .type)
+            try histogram.encode(to: encoder)
+        case .timeslice(let timeslice):
+            try container.encode("timeslice", forKey: .type)
+            try timeslice.encode(to: encoder)
+        case .topHits(let topHits):
+            try container.encode("top_hits", forKey: .type)
+            try topHits.encode(to: encoder)
+        case .uniqueCount(let uniqueCount):
+            try container.encode("unique_count", forKey: .type)
+            try uniqueCount.encode(to: encoder)
+        case .max(let max):
+            try container.encode("max", forKey: .type)
+            try max.encode(to: encoder)
+        case .min(let min):
+            try container.encode("min", forKey: .type)
+            try min.encode(to: encoder)
+        case .average(let average):
+            try container.encode("average", forKey: .type)
+            try average.encode(to: encoder)
+        case .sum(let sum):
+            try container.encode("sum", forKey: .type)
+            try sum.encode(to: encoder)
+        case .generic(let generic):
+            try generic.encode(to: encoder)
         }
     }
 

4cc9fc72d96d6cb2914b788103eb64d437419ecb
repjarms <jeffarn12@gmail.com>
2019-10-04 17:03:52 -0400

    ci(watson credentials): encrypt updated credentials

--- Scripts/travis/test-Linux.sh
+++ Scripts/travis/test-Linux.sh
@@ -6,7 +6,7 @@ tar xzf swift-4.2.4-RELEASE-ubuntu16.04.tar.gz
 export PATH=swift-4.2.4-RELEASE-ubuntu16.04/usr/bin:$PATH
 
 # Decrypt credentials files
-openssl aes-256-cbc -K $encrypted_d84ac0b7eb5c_key -iv $encrypted_d84ac0b7eb5c_iv -in Source/SupportingFiles/WatsonCredentials.swift.enc -out Source/SupportingFiles/WatsonCredentials.swift -d
+openssl aes-256-cbc -K $encrypted_ba57191635e0_key -iv $encrypted_ba57191635e0_iv -in Source/SupportingFiles/WatsonCredentials.swift.enc -out Source/SupportingFiles/WatsonCredentials.swift -d
 openssl aes-256-cbc -K $encrypted_d84ac0b7eb5c_key -iv $encrypted_d84ac0b7eb5c_iv -in Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-input.json.enc -out Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-input.json -d
 openssl aes-256-cbc -K $encrypted_d84ac0b7eb5c_key -iv $encrypted_d84ac0b7eb5c_iv -in Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-output.json.enc -out Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-output.json -d
 

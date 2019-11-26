12a46fc7b289b580971da7c79672e2d223437740
repjarms <jeffarn12@gmail.com>
2019-11-26 09:49:30 -0500

    ci(credentials): update encrypted credentials

--- Scripts/travis/test-macOS.sh
+++ Scripts/travis/test-macOS.sh
@@ -6,7 +6,7 @@ brew update >/dev/null
 brew outdated carthage || brew upgrade carthage >/dev/null
 
 # Decrypt credentials files
-openssl aes-256-cbc -K $encrypted_d84ac0b7eb5c_key -iv $encrypted_d84ac0b7eb5c_iv -in Source/SupportingFiles/WatsonCredentials.swift.enc -out Source/SupportingFiles/WatsonCredentials.swift -d
+openssl aes-256-cbc -K $encrypted_451e23e8cd1f_key -iv $encrypted_451e23e8cd1f_iv -in Source/SupportingFiles/WatsonCredentials.swift.enc -out Source/SupportingFiles/WatsonCredentials.swift -d
 openssl aes-256-cbc -K $encrypted_d84ac0b7eb5c_key -iv $encrypted_d84ac0b7eb5c_iv -in Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-input.json.enc -out Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-input.json -d
 openssl aes-256-cbc -K $encrypted_d84ac0b7eb5c_key -iv $encrypted_d84ac0b7eb5c_iv -in Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-output.json.enc -out Tests/CompareComplyV1Tests/Resources/cloud-object-storage-credentials-output.json -d
 

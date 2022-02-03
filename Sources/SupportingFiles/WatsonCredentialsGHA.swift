d8d0d39bdb6352de2cf66578efacb7f17946ae40
Michael G Mosca <moscam@us.ibm.com>
2022-02-03 16:25:35 -0500

    ci: fix

--- Sources/SupportingFiles/WatsonCredentialsGHA.swift
+++ Sources/SupportingFiles/WatsonCredentialsGHA.swift
@@ -49,7 +49,7 @@ struct WatsonCredentials {
     static let NaturalLanguageClassifierPassword = "your-password-here"
     static let NaturalLanguageClassifierURL: String? = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com"
 
-    static let NaturalLanguageUnderstandingAPIKey: String? = ProcessInfo.processInfo.environment["NATURAL_LANGUAGE_UNDERSTANDING_APIKEY"]
+    static let NaturalLanguageUnderstandingAPIKey = ProcessInfo.processInfo.environment["NATURAL_LANGUAGE_UNDERSTANDING_APIKEY"]
     static let NaturalLanguageUnderstandingUsername = "your-username-here"
     static let NaturalLanguageUnderstandingPassword = "your-password-here"
     static let NaturalLanguageUnderstandingURL: String? = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com"
